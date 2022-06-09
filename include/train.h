cac20344cbfb9d53573f240255432d189512bbc4
Zeveke Marina <99339038+Ehrlich02@users.noreply.github.com>
2022-06-09 20:27:38 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,7 +1,6 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
-
 class Train {
  private:
   struct Cage {
@@ -9,21 +8,13 @@ class Train {
     Cage *next;
     Cage *prev;
   };
-
- int countOp, stepsForward;
- Cage *first, *current;
- Train::Cage* create(bool light) {
-  Cage* cage = new Cage;
-    cage->light = light;
-    cage->next = nullptr;
-    cage->prev = nullptr;
-    return cage;
- }
- 
+  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  Cage *first; // точка входа в поезд (первый вагон)
+  Cage *last;
  public:
-  Train() : countOp(0), first(nullptr), current(nullptr) {}
-  void addCage(bool);
-  int getLength();
-  int getOpCount();
+  Train();
+  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();          // вычислить длину поезда
+  int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
