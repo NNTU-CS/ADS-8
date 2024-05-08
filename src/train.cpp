e7de197ea3b41564784896e5806bf8a96062b73b
flapik19 <selivanovvlad3211@gmail.com>
2024-05-08 14:08:25 +0300

    cpplint

--- src/train.cpp
+++ src/train.cpp
@@ -2,65 +2,59 @@
 #include "train.h"
 
 Train::Train() {
-    countOp = 0;
-    first = nullptr;
+  countOp = 0;
+  first = nullptr;
 }
 
-void Train::addCage(bool light)
-{
-	Cage *newCage = new Cage;
-	newCage->light = light;
-	newCage->next = nullptr;
-	if (first == nullptr) {
-		first = newCage;
-		newCage->prev = nullptr;
-        first->next = first;
-	} else {
-		Cage* curr = first;
-		while (curr->next != first) {
-			curr = curr->next;
-		}
-		curr->next = newCage;
-		newCage->prev = curr;
-        newCage->next = first;
-        first->prev = newCage;
-	}
-}
-
-int Train::getLength()
-{
-	int len = 0; 
-	Cage *curr = first;
-	curr->light = true;
-	while (true) {
-		curr = curr->next;
-		len++;
-		countOp++;
-		if (curr->light == true) {
-			curr->light = false;
-			int maxLen = len;
-			len = 0;
-		    for (int i = 0; i < maxLen; i++) {
-			    curr = curr->prev;
-			    countOp++;
-		    }
-		    if (curr->light == false)
-			    return maxLen;
-	   }
+void Train::addCage(bool light) {
+  Cage *newCage = new Cage;
+  newCage->light = light;
+  newCage->next = nullptr;
+  if (first == nullptr) {
+    first = newCage;
+    newCage->prev = nullptr;
+    first->next = first;
+  } else {
+    Cage *curr = first;
+    while (curr->next != first) {
+      curr = curr->next;
     }
+    curr->next = newCage;
+    newCage->prev = curr;
+    newCage->next = first;
+    first->prev = newCage;
+  }
 }
 
-int Train::getOpCount()
-{
-	return countOp;
+int Train::getLength() {
+  int len = 0;
+  Cage *curr = first;
+  curr->light = true;
+  while (true) {
+    curr = curr->next;
+    len++;
+    countOp++;
+    if (curr->light == true) {
+      curr->light = false;
+      int maxLen = len;
+      len = 0;
+      for (int i = 0; i < maxLen; i++) {
+        curr = curr->prev;
+        countOp++;
+      }
+      if (curr->light == false)
+        return maxLen;
+    }
+  }
 }
 
-Train::~Train()
-{
-	Cage* curr = new Cage;
-	while (curr->next != nullptr) {
-		Cage* temp = curr;
-		curr = curr->next;
-		delete[] temp;
-	}
+int Train::getOpCount() { return countOp; }
+
+Train::~Train() {
+  Cage *curr = new Cage;
+  while (curr->next != nullptr) {
+    Cage *temp = curr;
+    curr = curr->next;
+    delete[] temp;
+  }
 }
