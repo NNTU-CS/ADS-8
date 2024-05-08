ba0dc71d6634c92a4835890063d23521999d63c0
flapik19 <selivanovvlad3211@gmail.com>
2024-05-08 13:35:33 +0300

    idk

--- include/train.h
+++ include/train.h
@@ -16,6 +16,6 @@ class Train {
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
-  Train::~Train();
+  ~Train();
 };
 #endif  // INCLUDE_TRAIN_H_
