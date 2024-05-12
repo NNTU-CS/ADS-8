844487c4e6c59aa0b016977e6c6133144d3a7628
VadimKochnev <donskoy_kek@mail.ru>
2024-05-12 20:22:02 +0300

    b259

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,41 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train(): first(nullptr), countOp(0) {}
+
+void Train::addCage(bool light) {
+  Cage* node = new Cage;
+  node->light = light;
+
+  if (first == nullptr) {
+    first = node;
+    node->prev = node;
+    node->next = node;
+  }
+
+  node->next = first;
+  node->prev = first->prev;
+  first->prev->next = node;
+  first->prev = node;
+}
+
+int Train::getLength() {
+    Cage light;
+    int length = 0;
+
+    if (first != nullptr) {
+        length = 1;
+        Cage* current = first->next;
+
+        while (current == first - 1) {
+            length++;
+            current = current->next;
+        }
+    }
+
+    return length;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
