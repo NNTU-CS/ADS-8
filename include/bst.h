f772285c6100f46cfcf7eed5e0781627dca6b875
Van4e7 <ivanzvectov@gmail.com>
2025-05-17 22:30:12 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-
+If you're reading this, omg. Yes, I'm doing this fucking ADS on a Saturday night, because I don't care what else to do(
 #endif  // INCLUDE_BST_H_
