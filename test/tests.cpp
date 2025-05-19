c8aba1bfb35d108862e785ef7c4c911bf3dffbc1
Van4e7 <ivanzvectov@gmail.com>
2025-05-19 17:00:10 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -13,21 +13,21 @@ TEST(lab8, test1_1) {
     EXPECT_EQ(depth, 35);
 }
 TEST(lab8, test1_2) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/src/war_peace.txt";
+    const char* filename = "war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("pierre");
     EXPECT_EQ(count, 1963);
 }
 TEST(lab8, test1_3) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/src/war_peace.txt";
+    const char* filename = "war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("natasha");
     EXPECT_EQ(count, 1212);
 }
 TEST(lab8, test1_4) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/src/war_peace.txt";
+    const char* filename = "war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("andrew");
