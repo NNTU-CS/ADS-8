434fd6f5d7f64cdf5d847b28e221ac2c8c055585
MisTerio497 <ipkovalenko2006@gmail.com>
2025-05-19 17:03:46 +0300

    1

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,61 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
-#endif  // INCLUDE_BST_H_
+#include <algorithm> // для std::max
+
+template <typename T> class BST {
+  struct Node {
+    Node *left;
+    Node *right;
+    int count;
+    T word;
+    Node(const T &word) : word(word), left(nullptr), right(nullptr), count(1) {}
+  };
+  Node *root;
+  void add(Node *&node, const T &word) {
+    if (!node) {
+      node = new Node(word);
+      return;
+    }
+    if (word > node->word) {
+      add(node->right, word);
+    } else if (word < node->word) {
+      add(node->left, word);
+    } else {
+      node->count++;
+    }
+  }
+  int treeHeight(Node *node) const {
+    if (!node)
+      return 0;
+    int leftHeight = treeHeight(node->left);
+    int rightHeight = treeHeight(node->right);
+    return std::max(leftHeight, rightHeight) + 1;
+  }
+  int search(Node *node, const T &word) const {
+    if (!node)
+      return 0;
+    if (word > node->word)
+      return search(node->right, word);
+    else if (word < node->word)
+      return search(node->left, word);
+    else
+      return node->count;
+  }
+  void free(Node *node) {
+    if (!node)
+      return;
+    free(node->left);
+    free(node->right);
+    delete node;
+  }
+
+public:
+  BST() : root(nullptr) {}
+  ~BST() { free(root); }
+  void add(const T &word) { add(root, word); }
+  int depth() const { return treeHeight(root); }
+  int search(const T &word) const { return search(root, word); }
+};
+
+#endif // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,28 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  "bst.h"
+#include  "../include/bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+      std::cerr << "Ошибка: не удалось открыть файл!" << std::endl;
+    }
+    std::string word;
+    char ch;
+  
+    while (file.get(ch)) {
+      if (std::isalpha(static_cast<unsigned char>(ch))) {
+        word += std::tolower(static_cast<unsigned char>(ch));
+      } else {
+        if (!word.empty()) {
+            tree.add(word);
+          word.clear();
+        }
+      }
+    }
+    if (!word.empty()) {
+        tree.add(word);
+    }
+    file.close();
 }
