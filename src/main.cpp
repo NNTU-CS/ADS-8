09f2bb83118a596158c0a5d0ddeec0808002f232
MisTerio497 <ipkovalenko2006@gmail.com>
2025-05-19 18:45:24 +0300

    4

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,4 @@
 // // Copyright 2021 NNTU-CS
-// #include "../include/bst.h"
-// int main() { return 0; }
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include <string>
-#include  <cstdlib>
-#include  "../include/bst.h"
+#include "../include/bst.h"
+int main() { return 0; }
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-      std::cerr << "Ошибка: не удалось открыть файл!" << std::endl;
-    }
-    std::string word;
-    char ch;
-
-    while (file.get(ch)) {
-      if (std::isalpha(static_cast<unsigned char>(ch))) {
-        word += std::tolower(static_cast<unsigned char>(ch));
-      } else {
-        if (!word.empty()) {
-            tree.add(word);
-          word.clear();
-        }
-      }
-    }
-    if (!word.empty()) {
-        tree.add(word);
-    }
-    file.close();
-}
-
-int main(){
-    const char* filename = "src/war_peace.txt";
-    BST<std::string> tree;
-    makeTree(tree, filename);
-    int depth = tree.search("pierre");
-    std::cout << depth;
-}
