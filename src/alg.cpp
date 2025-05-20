414650df94866b95b1ea358e473c2a965a7d60a9
Анна <sol0dova.anna@yandex.ru>
2025-05-20 23:25:50 +0300

    ads-8

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return;
+  }
+  std::string word;
+  while (!file.eof()) {
+    char ch = file.get();
+    if (file.eof()) break;
+    if (std::isalpha(static_cast<unsigned char>(ch))) {
+      word += std::tolower(ch);
+    } else if (!word.empty()) {
+      tree.insert(word);
+      word.clear();
+    }
+  }
+  if (!word.empty()) {
+    tree.insert(word);
+  }
+  file.close();
 }
