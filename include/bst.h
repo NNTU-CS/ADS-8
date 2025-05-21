3bf788d983a1af0c9e85d9e58f237c24bd49933e
Анна <sol0dova.anna@yandex.ru>
2025-05-21 23:59:28 +0300

    ads-8

--- include/bst.h
+++ include/bst.h
@@ -14,48 +14,49 @@ class BST {
 private:
     Node<T>* root;
     void insert(Node<T>*& node, const T& key) {
-      if (!node) {
-        node = new Node<T>(key);
-      } else if (key < node->key) {
-        insert(node->left, key);
-      } else if (key > node->key) {
-        insert(node->right, key);
-      } else {
-        node->count++;
-      }
+        if (!node) {
+            node = new Node<T>(key);
+        } else if (key < node->key) {
+            insert(node->left, key);
+        } else if (key > node->key) {
+            insert(node->right, key);
+        } else {
+            node->count++;
+        }
     }
     int depth(Node<T>* node) const {
-      if (!node) return 0;
-      int left = depth(node->left);
-      int right = depth(node->right);
-      int maxDepth = (left > right) ? left : right;
-      return maxDepth + 1;
+        if (!node) return 0;
+        int left = depth(node->left);
+        int right = depth(node->right);
+        return (left > right ? left : right) + 1;
     }
     Node<T>* search(Node<T>* node, const T& key) const {
-      if (!node || node->key == key) return node;
-      Node* nextNode = (key < node->key) ? node->left : node->right;
-      return search(nextNode, key);
+        if (!node || node->key == key) return node;
+        return key < node->key ? search(node->left, key) : search(node->right, key);
     }
     void inorder(Node<T>* node, void(*process)(const T&, int)) const {
-      if (!node) return;
-      inorder(node->left, process);
-      process(node->key, node->count);
-      inorder(node->right, process);
+        if (!node) return;
+        inorder(node->left, process);
+        process(node->key, node->count);
+        inorder(node->right, process);
     }
     void destroy(Node<T>* node) {
-      if (!node) return;
-      destroy(node->left);
-      destroy(node->right);
-      delete node;
+        if (!node) return;
+        destroy(node->left);
+        destroy(node->right);
+        delete node;
     }
+
 public:
     BST() : root(nullptr) {}
     ~BST() { destroy(root); }
+
     void insert(const T& key) { insert(root, key); }
     int depth() const { return depth(root); }
     bool search(const T& key) const { return search(root, key) != nullptr; }
-    void traverse(void(*p)(const T&, int)) const {
-      inorder(root, p);
+
+    void traverse(void(*process)(const T&, int)) const {
+        inorder(root, process);
     }
 };
 #endif  // INCLUDE_BST_H_
