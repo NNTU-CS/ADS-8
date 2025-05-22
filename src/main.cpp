6c1754b6df25ce28d342e3d9735427bf5ecda309
nefedovat <ntanya54321@gmail.com>
2025-05-23 00:35:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+
 #include <iostream>
+#include <string>
 
 int main() {
-    BST<std::string> tree;
-    makeTree(tree, "src/war_peace.txt");
-    printFreq(tree);
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  printFreq(tree);
+  return 0;
 }
