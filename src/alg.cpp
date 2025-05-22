8113487694e9312ac686c7723282133485110a13
Anitone <133121791+Anitone@users.noreply.github.com>
2025-05-22 14:52:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,46 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) return;
+
+  std::string word = "";
+  int character;
+  while ((ch = file.get()) != EOF) {
+    if (ch >= 'A' && ch <= 'Z') {
+      ch += 'a' - 'A';
+    }
+
+    if (ch >= 'a' && ch <= 'z') {
+      word += ch;
+    } else {
+      if (!word.empty()) {
+        tree.add(word);
+      }
+      word = "";
+    }
+  }
+  if (!word.empty()) {
+    tree.add(word);
+  }
+
+  file.close();
+  std::cout << tree.depth();
+}
+
+bool compareFrequencies(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+  return a.second > b.second;
+}
+
+void printFreq(BST<std::string>& tree) {
+  std::vector<std::pair<std::string, int>> frequencies = tree.getFrequencies();
+  std::sort(frequencies.begin(), frequencies.end(), compareFrequencies);
+  std::ofstream outputFile("result/freq.txt");
+  if (!outputFile.is_open()) return;
+  
+  for (const auto& pair : frequencies) {
+    std::cout << pair.first << " - " << pair.second << std::endl;
+    outputFile << pair.first << " - " << pair.second << std::endl;
+  }
+  outputFile.close();
 }
