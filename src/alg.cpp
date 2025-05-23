3f9586e4392d15080bc89b646b212ffa76ce8585
Andrey20-07 <mr.klach999@gmail.com>
2025-05-23 14:44:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,30 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+void makeTree(BST<std::string>& tree, const char* namefile) {
+  std::ifstream LevTolstoyFile(namefile);
+  if (!LevTolstoyFile.is_open()) {
+    std::cerr << "Error: " << namefile << std::endl;
+      return;
+    }
+  std::string words2 = "";
+  char symbol;
+  while (LevTolstoyFile.get(symbol)) {
+    if(isalpha(symbol)){
+      words2 += std::tolower(symbol);
+    } else {
+      if(!words2.empty()){
+      tree.insert(words2);
+      }
+      words2 = "";
+    }
+  }
+  if (!words2.empty()) {
+    tree.insert(words2);
+  }
+  LevTolstoyFile.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+  tree.printInOrder();
 }
