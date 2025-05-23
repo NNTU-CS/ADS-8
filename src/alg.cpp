9563c0a2ed0e2affd1488c7e40221360c800a511
Анна <sol0dova.anna@yandex.ru>
2025-05-24 00:26:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,5 @@
 // Copyright 2021 NNTU-CS
-//#include  <iostream>
-//#include  <fstream>
-//#include  <locale>
-//#include  <cstdlib>
-//#include <string>
+
 #include  "bst.h"
 
 #include <iostream>
@@ -24,40 +20,34 @@ std::string toLower(const std::string& str) {
 }
 
 bool isLetter(char c) {
-    return (c >= 'a' && c <= 'z');
+    return std::isalpha(c);
 }
 
 void makeTree(BST<std::string>& tree, const char* filename) {
     std::ifstream file(filename);
-
     if (!file) {
         std::cerr << "File error!" << std::endl;
         return;
     }
 
     std::string word;
-    std::string currentWord;
     char ch;
-
+    
     while (file.get(ch)) {
-        if (std::isalpha(ch)) { 
-            currentWord += ch;
-        }
-        else {
-            if (!currentWord.empty()) {
-                tree.insert(toLower(currentWord));
-                currentWord = "";
-            }
+        if (isLetter(ch)) {
+            word += std::tolower(ch);
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
         }
     }
 
-    if (!currentWord.empty()) {
-        tree.insert(toLower(currentWord));
+    if (!word.empty()) {
+        tree.insert(word);
     }
 
     file.close();
 }
-
 
 void printFreq(BST<std::string>& tree) {
     std::vector<std::pair<std::string, int>> frequencies = tree.getFrequencies();
@@ -71,7 +61,6 @@ void printFreq(BST<std::string>& tree) {
         std::cerr << "Error opening result file!" << std::endl;
         return;
     }
-
     for (const auto& pair : frequencies) {
         std::cout << pair.first << " " << pair.second << std::endl;
         outputFile << pair.first << " " << pair.second << std::endl;
