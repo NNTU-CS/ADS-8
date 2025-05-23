632886c322b70e003f897538360f684e8e556354
DianaAvanesova <132001627+DianaAvanesova@users.noreply.github.com>
2025-05-23 16:11:00 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -18,28 +18,23 @@ class BST {
   };
 
   Node* root;
-
   void clear(Node* node) {
     node ? (clear(node->left), clear(node->right), delete node) : void();
   }
-
   void insert(Node*& node, const T& key) {
     !node ? node = new Node(key) : 
     (key == node->key ? node->count++ : 
      (key < node->key ? insert(node->left, key) : insert(node->right, key)));
   }
-
   int Search(Node* node, const T& key) const {
     return !node ? 0 : 
            key == node->key ? node->count : 
            Search(key < node->key ? node->left : node->right, key);
   }
-
   int Depth(Node* node) const {
     return !node ? -1 : 
            1 + (Depth(node->left) > Depth(node->right) ? Depth(node->left) : Depth(node->right));
   }
-
   void inorder(Node* node, std::vector<std::pair<T, int>>& vec) const {
     if (node) {
       inorder(node->left, vec);
@@ -51,11 +46,9 @@ class BST {
  public:
   BST() : root(nullptr) {}
   ~BST() { clear(root); }
-
   void insert(const T& key) { insert(root, key); }
   int search(const T& key) const { return Search(root, key); }
   int depth() const { return Depth(root); }
-
   std::vector<std::pair<T, int>> getAll() const {
     std::vector<std::pair<T, int>> vec;
     inorder(root, vec);
