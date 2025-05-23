79faeeadc3b4737aefa33b660c19445184272d97
DianaAvanesova <132001627+DianaAvanesova@users.noreply.github.com>
2025-05-23 16:08:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
-#include <string>
+#include <iostream>
 #include "bst.h"
+
 void makeTree(BST<std::string>&, const char*);
-void printFreq(BST<std::string>& tree);
+void printFreq(BST<std::string>&);
 
 int main() {
-  const char* file = "D:/Study/С++/ADS-8/ADS-8/war_peace.txt";
   BST<std::string> tree;
-  makeTree(tree, file);
+  makeTree(tree, "war_peace.txt");
   printFreq(tree);
   return 0;
 }
