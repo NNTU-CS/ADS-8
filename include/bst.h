1330f73d054246d9359839c495f7f474b1eb51fb
icegergert-world <andrejkornev26@gmail.com>
2025-05-24 23:40:56 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -16,23 +16,10 @@ struct Node {
 
 template<typename T>
 class BST {
- private:
-    Node<T>* root;  // Приватный член класса
-
-    // Разрешаем доступ классу Helper ко всему, что доступно внутри BST
+private:
+    Node<T>* root;
     friend class Helper;
 
- public:
-    BST() : root(nullptr) {}  // Конструктор
-    ~BST() { clear(root); }   // Деструктор
-
-    bool search(const T&);
-    void insert(const T&);
-    int depth(Node<T>* node);
-    void inorderTraversal(Node<T>* node);
-    void clear(Node<T>* node);
-
- private:
     void clear(Node<T>* node) {
         if (node != nullptr) {
             clear(node->left);
@@ -40,70 +27,15 @@ class BST {
             delete node;
         }
     }
+
+public:
+    BST() : root(nullptr) {}
+    ~BST() { clear(root); }
+
+    bool search(const T&);
+    void insert(const T&);
+    int depth(Node<T>* node);
+    void inorderTraversal(Node<T>* node);
 };
-
-template<typename T>
-BST<T>::BST() : root(nullptr) {}
-
-template<typename T>
-BST<T>::~BST() { clear(root); }
-
-template<typename T>
-void BST<T>::clear(Node<T>* node) {
-    if (node != nullptr) {
-        clear(node->left);
-        clear(node->right);
-        delete node;
-    }
-}
-
-template<typename T>
-void BST<T>::insert(const T& word) {
-    auto* newNode = new Node<T>(word);
-    if (root == nullptr) {
-        root = newNode;
-    } else {
-        Node<T>* current = root;
-        while (true) {
-            if (word == current->key) {
-                ++current->freq;
-                break;
-            } else if (word < current->key && current->left == nullptr) {
-                current->left = newNode;
-                break;
-            } else if (word > current->key && current->right == nullptr) {
-                current->right = newNode;
-                break;
-            } else if (word < current->key) {
-                current = current->left;
-            } else {
-                current = current->right;
-            }
-        }
-    }
-}
-
-template<typename T>
-bool BST<T>::search(const T& word) {
-    Node<T>* current = root;
-    while (current != nullptr) {
-        if (word == current->key) {
-            return true;
-        } else if (word < current->key) {
-            current = current->left;
-        } else {
-            current = current->right;
-        }
-    }
-    return false;
-}
-
-template<typename T>
-int BST<T>::depth(Node<T>* node) {
-    if (node == nullptr) {
-        return 0;
-    }
-    return 1 + std::max(depth(node->left), depth(node->right));
-}
 
 #endif  // INCLUDE_BST_H_
