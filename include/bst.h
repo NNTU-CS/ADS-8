d7c61fecd385342ea526db87fadf4b6fa86d2aae
egorkann <minicraftnn@mail.ru>
2025-05-24 11:49:28 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -40,7 +40,7 @@ class BST {
   }
 
   int depth(Node* node) const {
-    if (!node) return -1;  
+    if (!node) return -1;
     int leftDepth = depth(node->left);
     int rightDepth = depth(node->right);
     return 1 + std::max(leftDepth, rightDepth);
