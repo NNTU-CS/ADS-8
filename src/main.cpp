558b1ad8ca91e30d60dbd9d707cc7f4d684f4ffd
t7cel <bachinalva@gmail.com>
2025-05-25 22:08:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <string>
-#include "alg.cpp"
 #include "bst.h"
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() { BST<std::string> wordTree;
   const char* filename = "src/war_peace.txt";
