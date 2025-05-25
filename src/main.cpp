5df9574cc1fef02b158ae6baae0aa00daa76538b
t7cel <bachinalva@gmail.com>
2025-05-25 20:05:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "bst.h"
 
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+
+    std::cout << "Depth of the tree: " << tree.depth() << std::endl;
+    printFreq(tree);
     return 0;
 }
