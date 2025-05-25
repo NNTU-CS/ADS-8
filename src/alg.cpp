79982320897c82c92c69eaa7db6861bb987391fd
t7cel <bachinalva@gmail.com>
2025-05-25 21:50:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,53 +1,63 @@
 // Copyright 2021 NNTU-CS
-#include <utility>
-#include <sstream>
 #include  <iostream>
-#include  <fstream>
+#include <string>
 #include <algorithm>
+#include <cctype>
+#include <vector>
+#include <sstream>
+#include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include <string>
-#include <vector>
-#include <cctype>
 #include  "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-      std::ifstream file(filename);
-      std::string line;
+std::string toLower(const std::string& str) {
+    std::string result = str;
+    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+    return result;
+}
 
+void makeTree(BST<std::string>& tree, const char* filename) {std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
         return;
     }
 
+    std::string line;
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string word;
         while (ss >> word) {
-            std::string processedWord = "";
-            for (char c : word) {
-                if (std::isalpha(c)) {
-                    processedWord += std::tolower(c);
-                }
-            }
+            word.erase(std::remove_if(word.begin(), word.end(), [](unsigned char c){
+                return !std::isalpha(c);
+            }), word.end());
 
-            if (!processedWord.empty()) {
-                tree.insert(processedWord);
+            if (!word.empty()) {
+                tree.insert(toLower(word));
             }
         }
     }
+
     file.close();
 }
 
 
-void printFreq(BST<std::string>& tree) {
-    std::vector<std::pair<std::string, int>> frequencies = tree.getWordFrequencies();
+bool compareFrequencies(
+const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+    return a.second > b.second;
+}
 
-    std::sort(frequencies.begin(), frequencies.end(), [](const auto& a, const auto& b) {
-        return a.second > b.second;
-    });
+
+void printFreq(BST<std::string>& tree) {
+    std::vector<std::pair<std::string, int>> frequencies = tree.getFrequencies();
+
+    std::sort(frequencies.begin(), frequencies.end(), compareFrequencies);
 
     std::ofstream outfile("result/freq.txt");
+
+    if (!outfile.is_open()) {
+        std::cerr << "Error opening file result/freq.txt for writing!" << std::endl;
+        return;
+    }
 
     for (const auto& pair : frequencies) {
         std::cout << pair.first << ": " << pair.second << std::endl;
