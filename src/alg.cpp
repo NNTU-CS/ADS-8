4160d8c96c899104bce3fe6bcb25b7b19593b1d0
41kur1nka <121097596+41kur1nka@users.noreply.github.com>
2025-05-26 17:34:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,12 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 #include <algorithm>
 #include <cctype>
 #include <string>
 #include <vector>
-
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
