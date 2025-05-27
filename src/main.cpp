8603345516cb91f08b69bb00ad2398641dc42bf0
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-27 19:42:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
-
+#include <iostream>
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    printFreq(tree);
     return 0;
 }
