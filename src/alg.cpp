106cfc29b769f4dfaf991dc15fdb48c34778c72f
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-05-27 20:20:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,29 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+void makeTree(BST<std::string>& tree, const char* filename) {  
+  std::ifstream file(filename);
+  if (!file) {
+      std::cout << "File error!" << std::endl;
+      return;
+  }
+  std::string currentWord;
+  while (!file.eof()) {
+      char ch = file.get();
+      if (isalpha(ch)) {
+          currentWord += tolower(ch);
+      } else {
+        if (!currentWord.empty()) {
+            tree.insert(currentWord);
+            currentWord.clear();
+        }
+      }
+  }
+ 
+  if (!currentWord.empty()) {
+      tree.insert(currentWord);
+  }
+    
+  file.close();
 }
+
