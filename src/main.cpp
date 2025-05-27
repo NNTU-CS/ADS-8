ad231070d957e4a5b558aec815e396e3d83331e5
pootaatoovarenaya <lilechekT@mail.ru>
2025-05-27 10:44:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
 #include <iostream>
 #include <string>
+#include "bst.h"
 
 // The program builds a binary search tree from a text file and outputs its depth and word frequency
 
@@ -9,7 +9,6 @@ void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-    return 0;
   const char* filename = "src/war_peace.txt";
   BST<std::string> tree;
 
@@ -21,4 +20,5 @@ int main() {
 
   return 0;
 }
+
 
