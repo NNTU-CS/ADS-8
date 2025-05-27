83f0ed11d4ffb0e5f57a6131b4803889f751c4c1
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-05-27 19:51:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "../include/bst.h"
 #include <iostream>
+#include <string>
 void makeTree(BST<std::string>&, const char*);
 void printFreq(BST<std::string>&);
 int main() {
