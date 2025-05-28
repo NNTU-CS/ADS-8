69cf6b3c7cc5131d1b684ac25d64bcba1fce45e6
angryflaren <136967840+angryflaren@users.noreply.github.com>
2025-05-28 20:15:43 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include "bst.h"
 
-// Прототипы функций, определенных в src/alg.cpp
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
