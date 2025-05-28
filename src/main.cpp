2253d0ebaf01291cdb002f537aed46173986f554
ettennad <a.derlukova@mail.ru>
2025-05-29 01:37:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include "bst.h"
+
+#include <iostream>
+#include <string>
 
 int main() {
   BST<std::string> tree;
   const char* filename = "src/war_peace.txt";
+
   makeTree(tree, filename);
   std::cout << "Tree depth: " << tree.depth() << std::endl;
+
   printFreq(tree);
+
   return 0;
 }
