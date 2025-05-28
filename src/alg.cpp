f7d29dd868e5f9d82bb16d7a713bae995cd49b2d
TechnoMagicc <leyzarof@gmail.com>
2025-05-28 22:03:35 +0300

    Add files via upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,39 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return;
+    }
+
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word += std::tolower(ch);
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+}
+void printFreq(BST<std::string>& tree) {
+    
+    std::vector<std::pair<std::string, int>> freqVec;
+
+    std::sort(freqVec.begin(), freqVec.end(),
+              [](const auto& a, const auto& b) { return a.second > b.second; });
+
+    std::ofstream os("result/freq.txt");
+    for (const auto& pair : freqVec) {
+        os << pair.first << ": " << pair.second << std::endl;
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+    os.close();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include "alg.cpp"
+
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "src/war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
     return 0;
 }
