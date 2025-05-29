90d72722668880376f992b7912bcd002dc3a4c11
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-05-29 22:07:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 
+#include <iostream>
+#include <string>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
   BST<std::string> tree;
   const char* filename = "src/war_peace.txt";
@@ -12,4 +18,5 @@ int main() {
 
   return 0;
 }
+
 
