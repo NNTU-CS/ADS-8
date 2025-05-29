97b5f36c33e27bf0e7fdfa5acad5656ae1329daf
netherNet <whit3ufo@gmail.com>
2025-05-29 22:58:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,25 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) std::cerr << "File error!";
+  std::string word;
+  while (!file.eof()) {
+    char ch = file.get();
+    if (('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z')) {
+      word.push_back(('A' <= ch && ch <= 'Z') ? ch + ('a' - 'A') : ch);
+    } else if (!word.empty()) {
+        tree.insert(word);
+        word.clear();
+    }
+  }
+  file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+  std::vector<std::pair<std::string, int>> vec = tree.toVector();
+  std::ofstream out("result/freq.txt");
+  for (auto& pr : vec)
+    out << pr.first << " " << pr.second << std::endl;
+  out.close();
 }
