920c5a3f260d867e22e03c806eaaa9d04c34e232
AnnaApatina <annaapatina2002@gmail.com>
2025-05-30 14:17:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <string>
+#include <locale>
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
+    std::locale::global(std::locale("C"));
     BST<std::string> tree;
     const char* filename = "war_peace.txt";
     makeTree(tree, filename);
