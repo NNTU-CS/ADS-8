bf0837e368e413bd108194040dae120e7d018f34
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-05-30 23:55:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,30 +6,36 @@
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  std::ifstream file(filename);
-  if (!file) {
-    std::cerr << "File error!" << std::endl;
-    return;
-  }
-
-  std::string currentWord;
-  char ch;
-  while (file.get(ch)) {
-    if (isalpha(ch)) {
-      currentWord += tolower(ch);
-    } else if (!currentWord.empty()) {
-      tree.insert(currentWord);
-      currentWord.clear();
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return;
     }
-  }
 
-  if (!currentWord.empty()) {
-    tree.insert(currentWord);
-  }
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word += std::tolower(ch);
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
 
-  file.close();
+    file.close();
 }
 
 void printFreq(BST<std::string>& tree) {
-  tree.printFreq();
+    std::ofstream outFile("result/freq.txt");
+    if (!outFile) {
+        std::cerr << "Could not open output file!" << std::endl;
+        return;
+    }
+    tree.printFreq(outFile);
+    tree.printFreq(std::cout);
+    outFile.close();
 }
