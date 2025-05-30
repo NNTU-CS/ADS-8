fe7f788526dec4c894965f0707a93e7ddc56f79c
Julia-Mamaeva <julia0601alex@icloud.com>
2025-05-30 21:43:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include "bst.h"
+#include <string>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 
 void makeTree(BST<std::string>& tree, const char* filename) {
   std::ifstream file(filename);
