8a3862a4d4d9b21ef13d31f2f6c762fa83f0efd1
TechnoMagicc <leyzarof@gmail.com>
2025-05-30 21:10:01 +0300

    Add files via upload

--- include/bst.h
+++ include/bst.h
@@ -5,19 +5,17 @@
 #include <iostream>
 #include <vector>
 #include <utility>
-
 template <typename T>
 struct Node {
     T value;
     int count;
     Node<T>* left;
     Node<T>* right;
-
-    Node(const T& val) : value(val), count(1), left(nullptr), right(nullptr) {}
+    explicit Node(const T& val) : value(val), count(1), left(nullptr), right(nullptr) {}
 };
 template <typename T>
 class BST {
-public:
+ public:
     BST() : root_(nullptr) {}
     void getWordsAndFrequencies(std::vector<std::pair<T, int>>& freqVec) const {
         getWordsAndFrequenciesRec(root_, freqVec);
@@ -35,7 +33,7 @@ public:
         printFreqRec(root_, os);
     }
 
-private:
+ private:
     Node<T>* root_;
     Node<T>* insertRec(Node<T>* node, const T& value) {
         if (node == nullptr) {
@@ -78,6 +76,7 @@ private:
         os << node->value << ": " << node->count << std::endl;
         printFreqRec(node->right, os);
     }
+
     void getWordsAndFrequenciesRec(const Node<T>* node, std::vector<std::pair<T, int>>& freqVec) const {
         if (node == nullptr) {
             return;
@@ -88,4 +87,4 @@ private:
     }
 };
 
-#endif // INCLUDE_BST_H_
+#endif  // INCLUDE_BST_H_
