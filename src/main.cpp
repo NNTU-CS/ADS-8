63cfe38dfbd54ac4da2ec20673b73459690cdffd
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-05-31 00:30:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2021 NNTU-CS
 #include <string>
+#include <iostream>
 #include "bst.h"
+
+extern void makeTree(BST<std::string>& tree, const char* filename);
+extern void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
@@ -11,4 +15,3 @@ int main() {
 
     std::cout << "Tree depth: " << tree.depth() << std::endl;
     return 0;
-}
