a65e8bb5e1815ca5a04fd1872769ed20172836d4
Julia-Mamaeva <julia0601alex@icloud.com>
2025-05-30 21:30:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <fstream>
+
+void makeTree(BST<std::string>& wordTree, const char* filename);
+void printFreq(BST<std::string>& wordTree);
 
 int main() {
+    BST<std::string> frequencyTree;
+    const char* novelFile = "war_peace.txt";
+
+    makeTree(frequencyTree, novelFile);
+
+    std::ofstream resultFile("result/freq.txt");
+    if (resultFile) {
+        frequencyTree.printReverseInOrder(resultFile);
+        resultFile.close();
+    }
+
     return 0;
 }
