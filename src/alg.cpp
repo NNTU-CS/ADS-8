24f064e0fc2281b279b30360d93f860287cc773e
AnnaApatina <annaapatina2002@gmail.com>
2025-05-30 14:41:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,24 +9,6 @@
 #include  <string>
 #include  "bst.h"
 
-template<typename T>
-void sortRec(BST<T>& t, const std::vector<T>& w, int l, int r) {
-  if (l > r)
-    return;
-  int mid = (l + r) / 2;
-  t.add(w[mid]);
-  sortRec(t, w, l, mid - 1);
-  sortRec(t, w, mid + 1, r);
-}
-template<typename T>
-void addSorted(BST<T>& t, const std::vector<T>& w) {
-  if (w.empty())
-    return;
-  std::vector<T> sortW = w;
-  std::sort(sortW.begin(), sortW.end());
-  sortRec(t, sortW, 0, sortW.size() - 1);
-}
-
 void makeTree(BST<std::string>& tree, const char* filename) {
   std::ifstream file(filename);
   if (!file) {
@@ -35,21 +17,19 @@ void makeTree(BST<std::string>& tree, const char* filename) {
   }
   std::string word;
   int ch;
-  std::vector<std::string> words;
   while ((ch = file.get()) != EOF) {
     if (std::isalpha(ch)) {
       word += static_cast<char>(std::tolower(ch));
     } else {
       if (!word.empty()) {
-        words.push_back(word);
+        tree.add(word);
         word.clear();
       }
     }
   }
   if (!word.empty())
-    words.push_back(word);
+    tree.add(word);
   file.close();
-  addSorted(tree, words);
 }
 void printFreq(BST<std::string>& tree) {
   std::vector<std::pair<std::string, int>> result;
