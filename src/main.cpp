93ab244a6854f5cc9d1d66c74c7a3d302dbd093b
AnnaApatina <annaapatina2002@gmail.com>
2025-05-30 12:54:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
+#include "alg.cpp"
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
     return 0;
 }
