f07324e3325903ba3e55289614a45b34197e512d
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-05-30 22:14:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <fstream>
 #include "bst.h"
 
 int main() {
+    BST<std::string> wordTree;
+    const char* filename = "war_peace.txt";
+    
+    makeTree(wordTree, filename);
+    
+    std::ofstream outFile("result/freq.txt");
+    if (outFile) {
+        wordTree.printFreq(outFile);
+        outFile.close();
+    } else {
+        std::cerr << "Could not open output file!" << std::endl;
+    }
+    
+    std::cout << "Tree depth: " << wordTree.depth() << std::endl;
     return 0;
 }
