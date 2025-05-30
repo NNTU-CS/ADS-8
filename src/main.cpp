9ca5ad3256ec261cdb3e1fa2154de5dcf8926ef9
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-05-30 23:00:31 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    BST<std::string> wordTree;
-    const char* filename = "war_peace.txt";
-    
-    makeTree(wordTree, filename);
-    
-    std::ofstream outFile("result/freq.txt");
-    if (outFile) {
-        wordTree.printFreq(outFile);
-        outFile.close();
-    } else {
-        std::cerr << "Could not open output file!" << std::endl;
-    }
-    
-    std::cout << "Tree depth: " << wordTree.depth() << std::endl;
-    return 0;
+  BST<std::string> wordTree;
+  const char* filename = "war_peace.txt";
+  
+  makeTree(wordTree, filename);
+  
+  std::ofstream outFile("result/freq.txt");
+  if (outFile) {
+    wordTree.printFreq(outFile);
+    outFile.close();
+  } else {
+    std::cerr << "Could not open output file!" << std::endl;
+  }
+  
+  std::cout << "Tree depth: " << wordTree.depth() << std::endl;
+  return 0;
 }
