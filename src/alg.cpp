11bd6fba58a9a1d59fdf9f77b3a00f9e96d6cbf0
Julia-Mamaeva <julia0601alex@icloud.com>
2025-05-30 21:29:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,28 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream fileStream(filename);
+    if (!fileStream) {
+        std::cerr << "Error opening file!" << std::endl;
+        return;
+    }
+
+    std::string tempWord;
+    char currentChar;
+    while (fileStream.get(currentChar)) {
+        if (isalpha(currentChar)) {
+            tempWord += tolower(currentChar);
+        } else if (!tempWord.empty()) {
+            wordTree.insert(tempWord);
+            tempWord.clear();
+        }
+    }
+    if (!tempWord.empty()) {
+        wordTree.insert(tempWord);
+    }
+    fileStream.close();
+}
+
+void printFreq(BST<std::string>& wordTree) {
+    wordTree.printReverseInOrder();
 }
