e8b166f2446fc11595ff2c13d84544da8fcd4f91
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-05-30 23:00:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,35 +1,35 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <fstream>
-#include <locale>
 #include <cctype>
+#include <string>
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "File error!" << std::endl;
-        return;
-    }
+  std::ifstream file(filename);
+  if (!file) {
+    std::cerr << "File error!" << std::endl;
+    return;
+  }
 
-    std::string currentWord;
-    char ch;
-    while (file.get(ch)) {
-        if (isalpha(ch)) {
-            currentWord += tolower(ch);
-        } else if (!currentWord.empty()) {
-            tree.insert(currentWord);
-            currentWord.clear();
-        }
+  std::string currentWord;
+  char ch;
+  while (file.get(ch)) {
+    if (isalpha(ch)) {
+      currentWord += tolower(ch);
+    } else if (!currentWord.empty()) {
+      tree.insert(currentWord);
+      currentWord.clear();
     }
+  }
 
-    if (!currentWord.empty()) {
-        tree.insert(currentWord);
-    }
+  if (!currentWord.empty()) {
+    tree.insert(currentWord);
+  }
 
-    file.close();
+  file.close();
 }
 
 void printFreq(BST<std::string>& tree) {
-    tree.printFreq();
+  tree.printFreq();
 }
