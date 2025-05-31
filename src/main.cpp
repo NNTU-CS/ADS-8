0bf2b8b9f554ee7a30f8586982e5bf4283f1fa68
Lina-Mar <martipoli7@gmail.com>
2025-05-31 21:43:46 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 #include <iostream>
+#include <string>
 
 int main() {
     BST<std::string> wordTree;
     const char* filename = "src/war_peace.txt";
-    
     makeTree(wordTree, filename);
-    
     std::cout << "Tree depth: " << wordTree.depth() << std::endl;
-    
     printFreq(wordTree);
-    
     return 0;
 }
