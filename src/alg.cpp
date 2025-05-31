a885adce7e357eafd933b462d3107104a25b3463
amidamAru <ivanadrei18@gmail.com>
2025-05-31 19:26:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
 #include <fstream>
 #include <cctype>
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
     std::ifstream fin(filename);
@@ -12,8 +12,10 @@ void makeTree(BST<std::string>& tree, const char* filename) {
         std::cerr << "Cannot open file: " << filename << "\n";
         return;
     }
+
     std::string word;
     char c;
+
     while (fin.get(c)) {
         if (std::isalpha(static_cast<unsigned char>(c))) {
             word += static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
@@ -22,29 +24,34 @@ void makeTree(BST<std::string>& tree, const char* filename) {
             word.clear();
         }
     }
+
     if (!word.empty()) {
         tree.add(word);
     }
+
     fin.close();
 }
+
 void printFreq(BST<std::string>& tree) {
     std::vector<std::pair<std::string, int>> words;
     tree.getAll(words);
+
     std::sort(words.begin(), words.end(),
-        [](const auto& a, const auto& b) {
-            return (a.second != b.second) ? a.second > b.second : a.first < b.first;
-        });
-    for (const auto& p : words) {
-        std::cout << p.first << " " << p.second << "\n";
-    }
+              [](const auto& a, const auto& b) {
+                  return (a.second != b.second) ? a.second > b.second : a.first < b.first;
+              });
+
     std::ofstream fout("result/freq.txt");
     if (fout.is_open()) {
         for (const auto& p : words) {
             fout << p.first << " " << p.second << "\n";
         }
         fout.close();
-    }
-    else {
+    } else {
         std::cerr << "Cannot create output file\n";
+    }
+
+    for (const auto& p : words) {
+        std::cout << p.first << " " << p.second << "\n";
     }
 }
