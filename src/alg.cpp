66ab73bf6a89bb178f326f9dbafbaac0ac39ddf3
yeefip <yeefipp@gmail.com>
2025-05-31 23:02:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,53 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <map>
+#include <algorithm>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return;
+    }
+
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word += std::tolower(ch);
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+    auto vec = tree.toVector();
+    std::sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second;
+    });
+
+    std::ofstream out("result/freq.txt");
+    if (!out) {
+        std::cerr << "Can't write to result/freq.txt" << std::endl;
+        return;
+    }
+
+    for (const auto& [word, count] : vec) {
+        std::cout << word << " — " << count << '\n';
+        out << word << " — " << count << '\n';
+    }
+
+    out.close();
 }
