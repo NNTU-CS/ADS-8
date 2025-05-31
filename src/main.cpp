ddfe1f9f3dc97ca10d63f51757fc798939bab19b
t7cel <bachinalva@gmail.com>
2025-05-31 22:51:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
-int main() { BST<std::string> wordTree;
+int main() {
   const char* filename = "src/war_peace.txt";
+  BST<std::string> wordTree;
 
   makeTree(wordTree, filename);
 
