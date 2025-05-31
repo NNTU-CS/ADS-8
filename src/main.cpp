fe7a6cb9a130ebf3bf06726f45363527aca6e7b1
jafar khadour <jkhadour111@gmail.com>
2025-05-31 21:40:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
 
 int main() {
+    BST<std::string> wordTree;
+    makeTree(wordTree, "war_peace.txt");
+    printFreq(wordTree);
     return 0;
 }
