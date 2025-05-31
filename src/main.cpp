5f8b9d284b1a379213a091f413f89f59e04b9059
yeefip <yeefipp@gmail.com>
2025-06-01 00:07:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+
 int main() {
-    BST<std::string> tree;
-    tree.insert("hello");
-    tree.insert("world");
-    tree.insert("foo");
-    tree.insert("bar");
+  BST<std::string> tree;
+  const char* filename = "src/war_peace.txt";
+  makeTree(tree, filename);
 
-    std::cout << "Tree depth: " << tree.depth() << std::endl;  // здесь было Depth() — исправила
-    std::cout << "Tree size: " << tree.size() << std::endl;
+  auto wordsVec = tree.toVector();
+  std::sort(wordsVec.begin(), wordsVec.end(),
+            [](const auto& a, const auto& b) {
+              return a.second > b.second;
+            });
 
-    return 0;
+  std::ofstream out("result/freq.txt");
+  for (const auto& pair : wordsVec) {
+    std::cout << pair.first << " - " << pair.second << std::endl;
+    out << pair.first << " - " << pair.second << std::endl;
+  }
+
+  return 0;
 }
