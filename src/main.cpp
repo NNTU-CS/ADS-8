60f942daca9cc4c5809470eaa740d57716030286
jariuock <garanin.019@gmail.com>
2025-05-31 14:18:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include <iostream>
 #include <string>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-    BST<std::string> tree;
-    makeTree(tree, "src/war_peace.txt");
-    printFreq(tree);
+    const char* inputFile = "src/war_peace.txt";
+    BST<std::string> wordTree;
+
+    makeTree(wordTree, inputFile);
+
+    std::cout << "Tree depth (edges): " << wordTree.depth() << "\n\n";
+    printFreq(wordTree);
+
     return 0;
 }
