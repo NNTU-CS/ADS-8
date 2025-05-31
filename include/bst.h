c368e2292aa1e2710dca3b74efc894ca08c734cc
KorobovaKate <kat3kor@yandex.ru>
2025-05-31 20:59:34 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,90 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+template <typename T>
+class BST {
+private:
+  struct Node {
+        T slovo;
+        int kol;
+        Node* left;
+        Node* right;
+        Node(T s) : slovo(s), kol(1), left(nullptr), right(nullptr) {}
+    };
+  Node* root;
+  Node* addNode(Node* spot, T value) {
+    if (spot == nullptr) {
+      Node* newNode = new Node(value);
+      return newNode;
+    } else if (value < spot->slovo) {
+      spot->left = addNode(spot->left, value);
+    } else if (value > spot->slovo) {
+      spot->right = addNode(spot->right, value);
+    } else {
+      spot->kol++;
+    }
+    return spot;
+  }
+
+  int heightT(Node* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    int leftH = heightT(root->left);
+    int rightH = heightT(root->right);
+    return std::max(leftH, rightH) + 1;
+  }
+
+  int searchNode(Node* spot, T value) const {
+    if (spot == nullptr) {
+        return 0;
+    } else if (value < spot->slovo) {
+        return searchNode(spot->left, value);
+    } else if (value > spot->slovo) {
+        return searchNode(spot->right, value);
+    } else return spot->kol;
+  }
+
+  void obhod(Node* node, Visitor visit) const {
+    if (node != nullptr) {
+      obhod(node->left, visit);
+      visit(node);
+      obhod(node->right, visit);
+    }
+  }
+
+  void delet(Node* root) {
+    if(root == nullptr)
+      return;
+    delet(root->left);
+    delet(root->right);
+    delete root;
+  }
+
+public:
+  BST() : root(nullptr) {}
+  ~BST() { clear(root); }
+
+  void addNode(T value) {
+    root = addNode(root, value);
+  }
+
+    int heightT() const {
+        return heightT(root);
+    }
+
+    Node* searchNode(T value) const {
+        return searchNode(root, value);
+    }
+
+    void obhod(void (*visit)(Node*)) const {
+        obhod(root, visit);
+    }
+}
+
 #endif  // INCLUDE_BST_H_
