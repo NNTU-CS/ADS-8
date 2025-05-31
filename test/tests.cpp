bdbd4932f8b329e35a2f178844adc719e47fbe7c
KorobovaKate <kat3kor@yandex.ru>
2025-05-31 21:27:55 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,27 +9,27 @@ TEST(lab8, test1_1) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int depth = tree.heightT();
+    int depth = tree.depth();
     EXPECT_EQ(depth, 35);
 }
 TEST(lab8, test1_2) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int count = tree.searchNode("pierre");
+    int count = tree.search("pierre");
     EXPECT_EQ(count, 1963);
 }
 TEST(lab8, test1_3) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int count = tree.searchNode("natasha");
+    int count = tree.search("natasha");
     EXPECT_EQ(count, 1212);
 }
 TEST(lab8, test1_4) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
-    int count = tree.searchNode("andrew");
+    int count = tree.search("andrew");
     EXPECT_EQ(count, 1143);
 }
