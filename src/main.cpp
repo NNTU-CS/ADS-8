fc3af11f4d422b1534bd30189e3a1d4416f32d5b
yeefip <yeefipp@gmail.com>
2025-06-01 00:10:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,12 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <algorithm>
 #include "bst.h"
-
-void makeTree(BST<std::string>& tree, const char* filename);
+#include "alg.h"
+#include <iostream>
 
 int main() {
-  BST<std::string> tree;
-  const char* filename = "src/war_peace.txt";
-  makeTree(tree, filename);
-
-  auto wordsVec = tree.toVector();
-  std::sort(wordsVec.begin(), wordsVec.end(),
-            [](const auto& a, const auto& b) {
-              return a.second > b.second;
-            });
-
-  std::ofstream out("result/freq.txt");
-  for (const auto& pair : wordsVec) {
-    std::cout << pair.first << " - " << pair.second << std::endl;
-    out << pair.first << " - " << pair.second << std::endl;
-  }
-
-  return 0;
+    BST<std::string> tree;
+    process("data.txt", tree);
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    std::cout << "Unique words: " << tree.count() << std::endl;
+    return 0;
 }
