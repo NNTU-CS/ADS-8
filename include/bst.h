dea07e469528699bf423c722e0c4af5484736c14
yeefip <yeefipp@gmail.com>
2025-06-01 00:19:51 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -7,6 +7,9 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <cctype>
 
 template <typename T>
 class BST {
@@ -97,5 +100,24 @@ class BST {
     return freqList;
   }
 };
+
+template <typename T>
+void process(const std::string& filename, BST<T>& tree) {
+  std::ifstream file(filename);
+  std::string line, word;
+
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    while (iss >> word) {
+      word.erase(std::remove_if(word.begin(), word.end(),
+        [](unsigned char c) { return !std::isalpha(c); }),
+        word.end());
+      std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+      if (!word.empty()) {
+        tree.insert(word);
+      }
+    }
+  }
+}
 
 #endif  // INCLUDE_BST_H_
