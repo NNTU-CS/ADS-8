275be9ef84f7efdb6faffd4e45b7bae8b0cc80f8
KorobovaKate <kat3kor@yandex.ru>
2025-05-31 22:21:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include "alg.cpp"
 
 int main() {
+    BST<std::string> wordTree;
+    const char* filename = "src/war_peace.txt";
+
+    makeTree(wordTree, filename);
+
+    std::cout << "Tree height: " << wordTree.heightT() << std::endl;
+
+    printFreq(wordTree);
+
+    int kol = wordTree.searchNode("in");
+    if (ko; > 0) {
+        std::cout << "Word 'in' found with count: " << count << std::endl;
+    } else {
+        std::cout << "Word 'in' not found" << std::endl;
+    }
+
     return 0;
 }
