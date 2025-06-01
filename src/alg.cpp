8b9782b8deb1a7f6e93e24eec493adda133fe22b
Эль шейх <elina_shibanova@mail.ru>
2025-06-01 21:35:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,12 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 #include <cctype>
 #include <vector>
 #include <algorithm>
+#include <string>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
