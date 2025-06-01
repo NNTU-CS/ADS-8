60641656ee9d9446e74053fd2f25e8e774744d8a
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-06-01 16:37:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+#include <string>
+
+extern void makeTree(BST<std::string>& tree, const char* filename);
+extern void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> wordTree;
+    const char* inputFile = "src/war_peace.txt";
+
+    makeTree(wordTree, inputFile);
+    printFreq(wordTree);
+
+    std::cout << "Depth of the BST: " << wordTree.depth() << std::endl;
+
     return 0;
 }
