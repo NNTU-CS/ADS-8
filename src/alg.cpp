fac8cb5d0cc516efe15845a9ad53d1d70da88679
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-06-01 17:01:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,40 +1,39 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
 #include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <locale>
+
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-    std::ifstream file(filename);
+  std::ifstream file(filename);
 
-    if (!file) {
-        std::cerr << "File error!" << std::endl;
-        return;
-    }
+  if (!file) {
+    std::cerr << "File error!" << std::endl;
+    return;
+  }
 
-    std::string word;
-    int ch;
+  std::string word;
+  int ch;
 
-    while ((ch = file.get()) != EOF) {
-        if (std::isalpha(ch)) {
-            word += static_cast<char>(std::tolower(ch));
-        } else {
-            if (!word.empty()) {
-                tree.insert(word);
-                word.clear();
-            }
-        }
-    }
-
-    if (!word.empty()) {
+  while ((ch = file.get()) != EOF) {
+    if (std::isalpha(ch)) {
+      word += static_cast<char>(std::tolower(ch));
+    } else {
+      if (!word.empty()) {
         tree.insert(word);
+        word.clear();
+      }
     }
+  }
 
-    file.close();
+  if (!word.empty()) {
+    tree.insert(word);
+  }
+
+  file.close();
 }
 
-void printFreq(BST<std::string>& tree) {
-    tree.printToFileAndConsole();
-}
+void printFreq(BST<std::string>& tree) { tree.printToFileAndConsole(); }
