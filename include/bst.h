e3f40b4ff6d69d45107763fe7d863e91309d868e
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-06-01 14:58:34 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -17,10 +17,38 @@ Node* right; ///
 explicit Node(const T& elem) : element(elem), cnt(1), left(nullptr), right(nullptr) {}
 };
 Node* root = nullptr;
-Node* addNode(Node* node, const T& value);
-Node* searchNode(Node* node, const T& value) const;
-void inorderTraversal(Node* node, std::vector<std::pair<T, int>>& result) const;
-int calcDepth(Node* node) const;
+Node* addNode(Node* node, const T& value) {
+if (!node) return new Node(value);
+if (value < node->element) {
+node->left = addNode(node->left, value);
+} else if (value > node->element) {
+node->right = addNode(node->right, value);
+} else {
+node->cnt++;
+}
+return node;
+}
+
+Node* searchNode(Node* node, const T& value) const{
+if (!node || node->element == value) {
+return node;
+}
+return value < node->element ? searchNode(node->left, value) : searchNode(node->right, value);
+}
+void inorderTraversal(Node* node, std::vector<std::pair<T, int>>& result) const {
+if (node) {
+inorderTraversal(node->right, result);
+result.emplace_back(node->element, node->cnt);
+inorderTraversal(node->left, result);
+}
+}
+int calcDepth(Node* node) const {
+if (!node) return 0;
+int leftDepth = calcDepth(node->left);
+int rightDepth = calcDepth(node->right);
+return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
+
 void clearTree(Node* node) {
 if (node) {
 clearTree(node->left);
