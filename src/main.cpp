9232492624713850c24136a64ca04889bc498b4b
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-06-01 14:06:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
 
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  printFreq(tree);
+
+  std::cout << "Depth: " << tree.depth() << std::endl;
+  std::cout << "Occurrences of 'war': " << tree.search("war") << std::endl;
+
+  return 0;
 }
