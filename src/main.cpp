9b03ea0590ab21a7c6d4c08ebab37d5ff58d0285
Sibilka <barsik05072005@gmail.com>
2025-06-01 17:18:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
-// Copyright 2025 NNTU-CS
-#include <iostream>
-#include <string>  
-#include "../include/bst.h"
+// Copyright 2021 NNTU-CS
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 int main() {
   BST<std::string> tree;
-  const char* filename = "src/war_peace.txt";
+  const char* filename = "war_peace.txt";
   makeTree(tree, filename);
-  std::cout << "Tree depth: " << tree.depth() << std::endl;
-  printFreq(tree);
-  return 0;
+  std::ofstream outFile("result/freq.txt");
+  if (!outFile) {
+    return EXIT_FAILURE;
+  }
+  tree.printReverseInOrder(outFile);
+  return EXIT_SUCCESS;
 }
