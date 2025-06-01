b5d01b45a79a238d97f1b89f5a6c8e55e0127738
Sibilka <barsik05072005@gmail.com>
2025-06-01 18:01:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include <string>
+#include <locale>
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-  const char* file = "src/war_peace.txt";
-  BST<std::string> tree;
-  makeTree(tree, file);
-  printFreq(tree);
-  return 0;
+    std::locale::global(std::locale("C"));
+    BST<std::string> tree;
+    const char* filename = "war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
+    return 0;
 }
