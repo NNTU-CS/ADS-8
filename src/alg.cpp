8fe1e392ae0ad42d157a547317ae000055009750
TechnoMagicc <leyzarof@gmail.com>
2025-06-01 14:11:37 +0300

    Add files via upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,52 +1,52 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <fstream>
-#include <cctype>
+#include <cctype>   
 #include <algorithm>
-#include "bst.h"
+#include "include/bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
     std::ifstream file(filename);
+    
     if (!file.is_open()) {
-        std::cerr << "Ошибка: не удалось открыть файл " << filename << std::endl;
+        std::cerr << "Ошибка: Не найден файл для анализа!" << std::endl;
         return;
     }
 
-    std::string current_word;
-    file.get();
-    char ch;
-    while (file.get(ch)) {
-        if (std::isalpha(ch)) {
-            current_word += std::tolower(ch);
+    std::string current_word{};
+    char current_char;
+    while (file.get(current_char)) {
+        if (std::isalpha(current_char)) {
+            current_word.push_back(std::tolower(current_char));
         } else {
             if (!current_word.empty()) {
-                tree.insert(current_word);
+                tree.insert(current_word.c_str());
                 current_word.clear();
             }
         }
     }
-    if (!current_word.empty()) {
+    if (!current_word.empty()) 
         tree.insert(current_word);
-    }
+    
     file.close();
 }
+
 void printFreq(BST<std::string>& tree) {
-    std::vector<std::pair<std::string, int>> freqVec;
-    tree.getWordsAndFrequencies(freqVec);
-    std::sort(freqVec.begin(), freqVec.end(), 
+
+    std::vector<std::pair<std::string, int>> items;
+    tree.getWordsAndFrequencies(items);
+    std::sort(items.begin(), items.end(), 
         [](const auto& a, const auto& b) {
-            return a.second > b.second || (a.second == b.second && a.first < b.first);
-        });
-    for (const auto& item : freqVec) {
-        std::cout << item.first << ": " << item.second << std::endl;
-    }
-    std::ofstream outfile("result/freq.txt");
-    if (outfile) {
-        for (const auto& item : freqVec) {
-            outfile << item.first << ": " << item.second << std::endl;
+            return (a.second > b.second) || 
+                (a.second == b.second && a.first < b.first);
         }
+    );
+    std::ofstream out("result/freq.txt");
+    
+    for (const auto& pair : items) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+        out << pair.first << ": " << pair.second << std::endl;
     }
-    else {
-        std::cerr << "Не могу открыть файл result/freq.txt для записи." << std::endl;
-    }
+
+    if (out) out.close();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "include/bst.h"
+
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
 
 int main() {
-    BST<std::string> freq_tree;
-    makeTree(freq_tree, "src/war_peace.txt");
-    std::cout << "Tree depth: " << freq_tree.depth() << std::endl;
-    printFreq(freq_tree);
+    BST<std::string> my_tree;
+
+    makeTree(my_tree, "src/war_peace.txt");
+    printFreq(my_tree);
+
     return 0;
 }
