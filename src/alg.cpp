d7b73d02c9f4be5d19eff66ec0f5f5a1d66e0b35
Spectxr <leeibov@gmail.com>
2025-06-01 20:19:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,8 +18,8 @@ void makeTree(BST<std::string>& tree, const char* filename) {
     std::string word;
     char ch;
     while (file.get(ch)) {
-        if (std::isalpha(static_cast<unsigned char>(ch))) {
-            word += static_cast<char>(std::tolower(ch));
+        if (std::isalpha(static_cast<unsigned char>(ch)) && ch < 128) {
+            word += std::tolower(ch);
         } else if (!word.empty()) {
             tree.add(word);
             word.clear();
