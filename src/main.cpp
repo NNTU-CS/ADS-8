e705750c764b203fcc302a5f65645d0cb80a3cd3
vlmrtn <nmartin_02@mail.ru>
2025-06-01 13:09:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
-
+#include "alg.cpp"
 int main() {
+    BST<std::string> wordTree;
+    const char* filename = "src/war_peace.txt";
+
+    makeTree(wordTree, filename);
+    printFreq(wordTree);
+
     return 0;
 }
