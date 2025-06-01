4df3567e2a38e5c7dba7f5db4bfc7bd4a49f19c3
Sibilka <barsik05072005@gmail.com>
2025-06-01 18:01:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,47 +1,43 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
-#include <string>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include <algorithm>
+#include  <vector>
+#include  <cctype>
+#include  <algorithm>
+#include  <string>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
   std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "File ERR" << std::endl;
-        return;
-    }
-
-    std::string cword;
-    char cch;
-
-    while (file.get(cch)) {
-        if (std::isalpha(cch)) {
-            cword += std::tolower(cch);
-        } else if (!cword.empty()) {
-            tree.insert(cword);
-            cword.clear();
-        }
-    }
-
-    if (!cword.empty()) {
-        tree.insert(cword);
-    }
-
-    file.close();
-}
-
-void printFreq(BST<std::string>& tree) {
-  auto wordsVec = tree.toVector();
-  std::sort(wordsVec.begin(), wordsVec.end(),
-            [](const auto& a, const auto& b) {
-              return a.second > b.second;
-            });
-  std::ofstream out("result/freq.txt");
-  for (const auto& pair : wordsVec) {
-    std::cout << pair.first << " - " << pair.second << std::endl;
-    out << pair.first << " - " << pair.second << std::endl;
+  if (!file) {
+    std::cerr << "File opening error!" << std::endl;
+    return;
   }
+  std::string word;
+  int ch;
+  while ((ch = file.get()) != EOF) {
+    if (std::isalpha(ch)) {
+      word += static_cast<char>(std::tolower(ch));
+    } else {
+      if (!word.empty()) {
+        tree.add(word);
+        word.clear();
+      }
+    }
+  }
+  if (!word.empty())
+    tree.add(word);
+  file.close();
+}
+void printFreq(BST<std::string>& tree) {
+  std::vector<std::pair<std::string, int>> result;
+  tree.getSorted(result);
+  std::ofstream outFile("result/freq.txt");
+  for (const auto& pair : result) {
+    std::cout << pair.first << ": " << pair.second << std::endl;
+    outFile << pair.first << " " << pair.second << std::endl;
+  }
+  outFile.close();
 }
