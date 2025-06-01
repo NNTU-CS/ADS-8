fc69a2041c0882aeebf1047736fa142d78faaf01
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-06-01 16:57:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,16 @@
 #include <string>
 
 extern void makeTree(BST<std::string>& tree, const char* filename);
-extern void printFreq(BST<std::string>& tree);
+extern void displayFrequency(BST<std::string>& tree);
 
 int main() {
     BST<std::string> wordTree;
     const char* inputFile = "src/war_peace.txt";
 
     makeTree(wordTree, inputFile);
-    printFreq(wordTree);
+    displayFrequency(wordTree);
 
-    std::cout << "Depth of the BST: " << wordTree.depth() << std::endl;
+    std::cout << "Depth of the BST: " << wordTree.height() << std::endl;
 
     return 0;
 }
