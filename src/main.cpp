4bdb4a5e555fc4c859826cc6d1895b6dcb36e32d
ratatyq <n.mantorov@yandex.ru>
2025-06-01 23:55:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
+
 int main() {
+    BST<std::string> wordTree;
+    makeTree(wordTree, "war_peace.txt");
+    printFreq(wordTree);
     return 0;
 }
