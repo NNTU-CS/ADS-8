3925d28cc2d23f5bc6c7074fd1ffa65570e1e758
Nastya3456-fed <Nasty3256@yandex.ru>
2025-06-01 15:11:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include "bst.h"
 
 int main() {
+
+    BST<std::string> tree;
+
+    std::ifstream file("input.txt");
+
+
+    if (!file.is_open()) {
+        std::cerr << "Ne ydalos otkr fail" << std::endl;
+        return 1;
+    }
+
+
+    std::string line, word;
+    while (std::getline(file, line)) {
+        std::istringstream stream(line);
+        while (stream >> word) {
+            tree.insert(word);
+        }
+    }
+    file.close();
+
+    tree.inOrder([](BST<std::string>::Node* node) {
+        std::cout << node->key << " : " << node->count << std::endl;
+        });
+
+    std::cout << "Glubina Dereva: " << tree.depth() << std::endl;
+
     return 0;
+
+
 }
