1708560ef1d7ae4001394ea53b348a4b66fe5425
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-01 19:42:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,39 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+using namespace std;
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+void makeTree(BST& tree, const char* filename) {
+  ifstream file(filename);
+  if (!file.is_open()) {
+      cerr << "Cannot open file: " << filename << "\n";
+      return;
+   }
+  MyString curWord;
+  char ch;
+  while (file.get(ch)) {
+      if (isalpha(static_cast<unsigned char>(ch))) {
+        curWord.clear();
+        do {
+          curWord.append(tolower(static_cast<unsigned char>(ch)));
+        } while (file.get(ch) && isalpha(static_cast<unsigned char>(ch)));
+        tree.insert(curWord);
+       }
+  }
+  file.close();
+}
+
+void printPeriod(BST& tree) {
+    auto words = tree.getAll();
+    sort(words.begin(), words.end(),
+        [](const auto& word1, const auto& word2) {
+            if (word1.second != word2.second) {
+                return word1.second > word2.second;
+            } else {
+                return word1.first < word2.first;
+            }
+        });
+    for (const auto& pair : words) {
+        cout << pair.first.c_str() << " " << pair.second << "\n";
+    }
 }
