5cd1e87ffd6dd5cbd40c7127655570d26408fac0
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-01 19:47:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,39 +4,44 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-using namespace std;
+using std::cerr;
+using std::cout;
+using std::ifstream;
+using std::sort;
+using std::string;
 
 void makeTree(BST& tree, const char* filename) {
   ifstream file(filename);
   if (!file.is_open()) {
-      cerr << "Cannot open file: " << filename << "\n";
-      return;
-   }
+    cerr << "Cannot open file: " << filename << "\n";
+    return;
+  }
+  
   MyString curWord;
   char ch;
   while (file.get(ch)) {
-      if (isalpha(static_cast<unsigned char>(ch))) {
-        curWord.clear();
-        do {
-          curWord.append(tolower(static_cast<unsigned char>(ch)));
-        } while (file.get(ch) && isalpha(static_cast<unsigned char>(ch)));
-        tree.insert(curWord);
-       }
+    if (isalpha(static_cast<unsigned char>(ch))) {
+      curWord.clear();
+      do {
+        curWord.append(tolower(static_cast<unsigned char>(ch)));
+      } while (file.get(ch) && isalpha(static_cast<unsigned char>(ch)));
+      tree.insert(curWord);
+    }
   }
   file.close();
 }
 
 void printPeriod(BST& tree) {
-    auto words = tree.getAll();
-    sort(words.begin(), words.end(),
-        [](const auto& word1, const auto& word2) {
-            if (word1.second != word2.second) {
-                return word1.second > word2.second;
-            } else {
-                return word1.first < word2.first;
-            }
-        });
-    for (const auto& pair : words) {
-        cout << pair.first.c_str() << " " << pair.second << "\n";
-    }
+  auto words = tree.getAll();
+  sort(words.begin(), words.end(),
+      [](const auto& lhs, const auto& rhs) {
+        if (lhs.second != rhs.second) {
+          return lhs.second > rhs.second;
+        } else {
+          return lhs.first < rhs.first;
+        }
+      });
+  for (const auto& pair : words) {
+    cout << pair.first.c_str() << " " << pair.second << "\n";
+  }
 }
