a7088ddaae7c3fd4f7d7a33266172cf5d3163dab
GLUKERZZZ <artem2801200635@gmail.com>
2025-06-01 20:20:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "../include/bst.h"
+#include <iostream>
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "C:/Users/Артём/Desktop/project/src/war_peace.txt";
+    makeTree(tree, filename);
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    printFreq(tree);
     return 0;
 }
