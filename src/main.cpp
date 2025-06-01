695da4a59699cd6127511e8ee07bf0842382ae22
vlmrtn <nmartin_02@mail.ru>
2025-06-01 17:15:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
-#include "bst.h"
 #include <string>
+#include "bst.h"
 int main() {
     BST<std::string> wordTree;
     const char* filename = "src/war_peace.txt";
