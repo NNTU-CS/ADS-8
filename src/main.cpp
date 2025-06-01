13888a10949a717b3cdabdee7478833f71875cca
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-01 19:53:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+void makeTree(BST&, const char*);
+void printPeriod(BST&);
 
 int main() {
+    BST tree;
+    makeTree(tree, "war_peace.txt");
+    printPeriod(tree);
     return 0;
 }
