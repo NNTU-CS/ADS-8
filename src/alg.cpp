89d456cbf18ed2ae737c6ab0a5dfd2d3c8408916
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-06-01 14:59:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,31 +10,6 @@
 #include  <cstring>
 #include  "bst.h"
 
-template<typename T>
-typename BST<T>::Node* BST<T>::searchNode(Node* node, const T& value) const {
-if (!node || node->element == value) {
-return node;
-}
-return value < node->element ? searchNode(node->left, value) : searchNode(node->right, value);
-}
-
-template<typename T>
-int BST<T>::calcDepth(Node* node) const {
-if (!node) return 0;
-int leftDepth = calcDepth(node->left);
-int rightDepth = calcDepth(node->right);
-return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
-}
-
-template<typename T>
-void BST<T>::inorderTraversal(Node* node, std::vector<std::pair<T, int>>& result) const {
-if (node) {
-inorderTraversal(node->right, result);
-result.emplace_back(node->element, node->cnt);
-inorderTraversal(node->left, result);
-}
-}
-
 void processFile(BST<std::string>& tree, std::ifstream& file) {
 char c;
 std::string currentWord;
@@ -77,17 +52,4 @@ std::cout << pair.first << ": " << pair.second << std::endl;
 outFile << pair.first << " " << pair.second << std::endl;
 }
 outFile.close();
-}
-
-template<typename T>
-typename BST<T>::Node* BST<T>::addNode(Node* node, const T& value) {
-if (!node) return new Node(value);
-if (value < node->element) {
-node->left = addNode(node->left, value);
-} else if (value > node->element) {
-node->right = addNode(node->right, value);
-} else {
-node->cnt++;
-}
-return node;
 }
