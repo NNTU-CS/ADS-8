cc16d7bdadf8b52120ec65c67f63e43eb346bb9d
KorobovaKate <kat3kor@yandex.ru>
2025-06-01 12:01:58 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -18,14 +18,14 @@ TEST(lab8, test1_2) {
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("pierre");
-    EXPECT_EQ(count, 1963);
+    EXPECT_EQ(count, 1955);
 }
 TEST(lab8, test1_3) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("natasha");
-    EXPECT_EQ(count, 1212);
+    EXPECT_EQ(count, 1201);
 }
 TEST(lab8, test1_4) {
     const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
