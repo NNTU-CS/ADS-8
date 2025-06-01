77ee2ef059715806f95012ccfe23afc34236493c
Sibilka <barsik05072005@gmail.com>
2025-06-01 17:41:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,33 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <fstream>
-#include <locale>
-#include <cstdlib>
-#include <string>
-#include <cctype>
-#include <vector>
-#include <algorithm>
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
 #include  "bst.h"
-std::string toLower(const std::string& str) {
-    std::string result = "";
-    for (char c : str) {
-        result += std::tolower(c);
-    }
-    return result;
-}
-bool isLetter(char c) {
-    return std::isalpha(c);
-}
+#include <string>
+
 void makeTree(BST<std::string>& tree, const char* filename) {
-    std::ifstream file(filename);
+  std::ifstream file(filename);
     if (!file) {
-        std::cerr << "File error!" << std::endl;
+        std::cerr << "File ERR" << std::endl;
         return;
     }
 
-    std::string word;
-    char ch;
-    while (file.get(ch)) {
-        if (isLetter(ch)) {
-            word += std::tolower(ch);
-        } else if (!word.empty()) {
-            tree.insert(word);
-            word.clear();
+    std::string cword;
+    char cch;
+
+    while (file.get(cch)) {
+        if (std::isalpha(cch)) {
+            cword += std::tolower(cch);
+        } else if (!cword.empty()) {
+            tree.insert(cword);
+            cword.clear();
         }
     }
-    if (!word.empty()) {
-        tree.insert(word);
+
+    if (!cword.empty()) {
+        tree.insert(cword);
     }
+
     file.close();
-}
-void printFreq(BST<std::string>& tree) {
-    std::vector<std::pair<std::string, int>> frequencies = tree.getFrequencies();
-    std::sort(frequencies.begin(), frequencies.end(), [](const auto& a, const auto& b) {
-        return a.second > b.second;
-        });
-    std::ofstream outputFile("freq.txt");
-    if (!outputFile) {
-        std::cerr << "Error opening result file!" << std::endl;
-        return;
-    }
-    for (const auto& pair : frequencies) {
-        std::cout << pair.first << " " << pair.second << std::endl;
-        outputFile << pair.first << " " << pair.second << std::endl;
-    }
-    outputFile.close();
 }
