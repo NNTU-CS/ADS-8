33539564e24fd5f314d40b459691f41dd5a165de
GLUKERZZZ <artem2801200635@gmail.com>
2025-06-01 20:35:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include "../include/bst.h"
+
 #include <iostream>
+#include <string>
 
 int main() {
-    BST<std::string> tree;
-    const char* filename = "C:/Users/Артём/Desktop/project/src/war_peace.txt";
-    makeTree(tree, filename);
-    std::cout << "Tree depth: " << tree.depth() << std::endl;
-    printFreq(tree);
-    return 0;
+  BST<std::string> tree;
+  const char* filename = "C:/Users/Артём/Desktop/project/src/war_peace.txt";
+  makeTree(tree, filename);
+  std::cout << "Tree depth: " << tree.depth() << std::endl;
+  printFreq(tree);
+  return 0;
 }
