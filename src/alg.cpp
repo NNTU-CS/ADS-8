3decc02dfda2f501de8df5a2878de1a96d4ff6e5
Sibilka <barsik05072005@gmail.com>
2025-06-01 18:13:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,41 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <vector>
+#include  <cctype>
+#include  <algorithm>
+#include  <string>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) {
+    std::cerr << "File opening error!" << std::endl;
+    return;
+  }
+  std::string word;
+  int ch;
+  while ((ch = file.get()) != EOF) {
+    if (std::isalpha(ch)) {
+      word += static_cast<char>(std::tolower(ch));
+    } else {
+      if (!word.empty()) {
+        tree.add(word);
+        word.clear();
+      }
+    }
+  }
+  if (!word.empty())
+    tree.add(word);
+  file.close();
+}
+void printFreq(BST<std::string>& tree) {
+  std::vector<std::pair<std::string, int>> result;
+  tree.getSorted(result);
+  std::ofstream outFile("result/freq.txt");
+  for (const auto& pair : result) {
+    std::cout << pair.first << ": " << pair.second << std::endl;
+    outFile << pair.first << " " << pair.second << std::endl;
+  }
+  outFile.close();
 }
