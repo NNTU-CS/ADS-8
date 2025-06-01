6bc4b507c414e4fd6cd32d3fa70b59b75b3265da
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-06-01 16:21:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,32 @@
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
+#include <string>
 #include  <cstdlib>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
+            word += std::tolower(static_cast<unsigned char>(ch));
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+    file.close();
 }
