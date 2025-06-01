18e09305a8614335bfc28d12ce6b4bf3bb472a78
Sibilka <barsik05072005@gmail.com>
2025-06-01 17:53:25 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,11 +1,13 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+
 #include <iostream>
 #include <utility>
 #include <string>
 #include <algorithm>
 #include <vector>
+
 template <typename T>
 class BST {
  private:
@@ -16,28 +18,35 @@ class BST {
         Node* right;
         explicit Node(const T& t) : val(t), ocCount(1), left(nullptr), right(nullptr) {}
     };
+
     Node* root;
+
     Node* add(Node* n, const T& t) {
         if (!n) {
             return new Node(t);
         }
+
         if (t == n->val) {
             n->ocCount++;
             return n;
         }
+
         if (t < n->val) {
             n->left = add(n->left, t);
         } else {
             n->right = add(n->right, t);
         }
+
         return n;
     }
+
     int depthTree(Node* n) const {
         if (!n) return -1;
         int lDepth = depthTree(n->left);
         int rDepth = depthTree(n->right);
         return 1 + (lDepth > rDepth ? lDepth : rDepth);
     }
+
     void clear(Node* n) {
         if (!n) return;
         clear(n->left);
@@ -55,9 +64,11 @@ class BST {
  public:
     BST() : root(nullptr) {}
     ~BST() { clear(root); }
+
     void insert(const T& value) {
         root = add(root, value);
     }
+
     int search(const T& value) const {
         Node* node = root;
         while (node) {
@@ -72,13 +83,16 @@ class BST {
         }
         return 0;
     }
+
     int depth() const {
         return depthTree(root);
     }
+
     std::vector<std::pair<T, int>> toVector() const {
       std::vector<std::pair<T, int>> res;
       ord(root, res);
       return res;
     }
 };
+
 #endif // INCLUDE_BST_H_
