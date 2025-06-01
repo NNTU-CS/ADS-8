ae311626dc41265a710c528261fb99e0aa7ce793
annChekh <anua.chekhonadskikh@mail.ru>
2025-06-01 22:47:46 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+#include <string>
 void makeTree(BST<std::string>&, const char* filename);
 void printPeriod(BST<std::string>& tree);
 
