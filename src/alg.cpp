5ff04d71e70c829064529e77046483ca22f8e974
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-06-01 16:59:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,37 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <cctype>
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return;
+    }
+
+    std::string word;
+    int ch;
+
+    while ((ch = file.get()) != EOF) {
+        if (std::isalpha(ch)) {
+            word += static_cast<char>(std::tolower(ch));
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+    tree.printToFileAndConsole();
 }
