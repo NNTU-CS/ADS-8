d6b8b158e8669383bfa01f036c5408821b168932
KMasha04 <k.masha04030420@gmail.com>
2025-06-02 22:41:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,21 +1,21 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 #include <string>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  std::ifstream file(filename);
-    BST<std::string> tree;
+    std::ifstream file(filename);
 
     if (file.is_open()) {
         std::string word;
         char symbol;
+
         while (file.get(symbol)) {
             if (std::isalpha(symbol)) {
-                word += std::tolower(symbol);
+                word += std::tolower(static_cast<unsigned char>(symbol));
             } else {
                 if (!word.empty()) {
                     tree.addNode(word);
@@ -32,6 +32,4 @@ void makeTree(BST<std::string>& tree, const char* filename) {
     } else {
         std::cout << "File was not read correctly." << std::endl;
     }
-
-    return tree;
 }
