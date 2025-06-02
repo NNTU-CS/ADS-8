a1c7758bd47c64f191ab832a587a5a603169e1a0
foltukol <foltukol@yandex.ru>
2025-06-02 17:36:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,6 +6,7 @@
 #include <cctype>
 #include <algorithm>
 #include <string>
+#include <vector>
 #include "bst.h"
 
 
@@ -20,15 +21,38 @@ void makeTree(BST<std::string>& tree, const char* filename) {
     while (file.get(ch)) {
       if (isalpha(static_cast<unsigned char>(ch))) {
         currentWord += tolower(static_cast<unsigned char>(ch));
-      } else if (!currentWord.empty()) {
-        tree.insert(currentWord);
+      } else {
+          if (!currentWord.empty() && currentWord.length() > 1) {
+            auto it = std::find_if(wordCounts.begin(), wordCounts.end(), [&](const auto& pair) { return pair.first == currentWord; });
+            if (it != wordCounts.end()) {
+              it->second++;
+            } else {
+              wordCounts.emplace_back(currentWord, 1);
+            }
+          }
         currentWord.clear();
       }
     }
-    if (!currentWord.empty()) {
-      tree.insert(currentWord);
+    if (!currentWord.empty() && currentWord.length() > 1) {
+      auto it = std::find_if(wordCounts.begin(), wordCounts.end(), [&](const auto& pair) { return pair.first == currentWord; });
+      if (it != wordCounts.end()) {
+        it->second++;
+      } else {
+      wordCounts.emplace_back(currentWord, 1);
+      }
     }
     file.close();
+    std::sort(wordCounts.begin(), wordCounts.end(), [](const auto& a, const auto& b) { return a.first < b.first; });
+    std::function<void(int, int)> balancedInsert = [&](int start, int end) {
+    if (start > end) return;
+      int mid = start + (end - start) / 2;
+      for (int i = 0; i < wordCounts[mid].second; ++i) {
+        tree.insert(wordCounts[mid].first);
+      }
+      balancedInsert(start, mid - 1);
+      balancedInsert(mid + 1, end);
+    };
+    balancedInsert(0, wordCounts.size() - 1);
 }
 
 void printFreq(const BST<std::string>& tree) {
