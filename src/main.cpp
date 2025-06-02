ab81bac3e3aa3166c8ce376f0f5bf8bace556903
foltukol <foltukol@yandex.ru>
2025-06-02 16:21:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+#include <string>
 
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "war_peace.txt");
+    printFreq(tree);
     return 0;
 }
