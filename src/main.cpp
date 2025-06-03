ea9a6347358d45e0a2b444c22e204653c35f303e
Egoriy <91011699+S1frech@users.noreply.github.com>
2025-06-03 12:03:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 
 #include "bst.h"
 
+// Объявляем функции перед использованием
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
   BST<std::string> tree;
   const char* filename = "src/war_peace.txt";
