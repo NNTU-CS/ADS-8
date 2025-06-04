09ff383ddaefc89e275364c7469009b19ce7612c
liguhska <masharostenko@mail.ru>
2025-06-04 14:26:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,13 @@
 int main() {
     return 0;
 }
+#include <iostream>
+
+int main() {
+	BST<std::string> tree;
+	const char* filename = "war_peace.txt";
+	makeTree(tree, filename);
+	std::cout << "Tree depth: " << tree.depth() << std::endl;
+	printFreq(tree);
+	return 0;
+}
