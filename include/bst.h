53d8823ceadd6c459bd2773933d59b6eb53cb367
KorobovaKate <kat3kor@yandex.ru>
2025-06-05 10:50:41 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,4 +1,4 @@
-// Copyright 2021 NNTU-CS
+// include/bst.h
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
@@ -9,18 +9,19 @@
 
 template <typename T>
 class BST {
- public:
-  struct Node {
+public:
+    struct Node {
         T slovo;
         int kol;
         Node* left;
         Node* right;
         explicit Node(T s) : slovo(s), kol(1), left(nullptr), right(nullptr) {}
-  };
+    };
 
- private:
-  Node* root;
-  Node* addNode(Node* root, const T& value) {
+private:
+    Node* root;
+
+    Node* addNode(Node* root, const T& value) {
         if (root == nullptr) {
             return new Node(value);
         }
@@ -34,64 +35,63 @@ class BST {
         return root;
     }
 
-  int heightT(Node* root) const {
-    if (root == nullptr) {
-      return 0;
+    int heightT(Node* root) const {
+        if (root == nullptr) {
+            return 0;
+        }
+        int leftH = heightT(root->left);
+        int rightH = heightT(root->right);
+        return std::max(leftH, rightH) + 1;
     }
-    int leftH = heightT(root->left);
-    int rightH = heightT(root->right);
-    return std::max(leftH, rightH) + 1;
-  }
 
-  int searchNode(Node* spot, T value) const {
-    if (spot == nullptr) {
-        return 0;
-    } else if (value < spot->slovo) {
-        return searchNode(spot->left, value);
-    } else if (value > spot->slovo) {
-        return searchNode(spot->right, value);
+    int searchNode(Node* spot, T value) const {
+        if (spot == nullptr) {
+            return 0;
+        } else if (value < spot->slovo) {
+            return searchNode(spot->left, value);
+        } else if (value > spot->slovo) {
+            return searchNode(spot->right, value);
+        }
+        return spot->kol;
     }
-    return spot->kol;
-  }
 
-  template <typename Visitor>
-  void obhod(Node* node, Visitor visit) const {
-    if (node != nullptr) {
-        obhod(node->left, visit);
-        visit(node);
-        obhod(node->right, visit);
+    template<typename Visitor>
+    void traverseInOrder(Node* node, Visitor visit) const {
+        if (node != nullptr) {
+            traverseInOrder(node->left, visit);
+            visit(node);
+            traverseInOrder(node->right, visit);
+        }
     }
-  }
 
-  void deleteT(Node* root) {
-    if (root == nullptr)
-      return;
-    deleteT(root->left);
-    deleteT(root->right);
-    delete root;
-  }
+    void clear(Node* root) {
+        if (root == nullptr)
+            return;
+        clear(root->left);
+        clear(root->right);
+        delete root;
+    }
 
- public:
-  struct Node;
-  BST() : root(nullptr) {}
-  ~BST() { deleteT(root); }
+public:
+    BST() : root(nullptr) {}
+    ~BST() { clear(root); }
 
-  void insert(T value) {
-    root = addNode(root, value);
-  }
+    void insert(const T& value) {
+        root = addNode(root, value);
+    }
 
-  int height() const {
-      return heightT(root);
-  }
+    int depth() const {
+        return heightT(root);
+    }
 
-  int search(T value) const {
-      return searchNode(root, value);
-  }
+    int search(const T& value) const {
+        return searchNode(root, value);
+    }
 
-  template <typename Visitor>
-  void inorder(Visitor visit) const {
-      obhod(root, visit);
-  }
+    template<typename Visitor>
+    void inorder(Visitor visit) const {
+        traverseInOrder(root, visit);
+    }
 };
 
 #endif  // INCLUDE_BST_H_
