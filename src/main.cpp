4fe9f5e5293cd83d3c8e4241ed06e079075a9e98
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-06-05 22:00:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
 #include <string>
 #include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc < 2) return 1;
   BST<std::string> tree;
-  const char* filename = "src/war_peace.txt";
-  makeTree(tree, filename);
+  makeTree(tree, argv[1]);
   printFreq(tree);
   return 0;
 }
