b9ff4a53695654105f5a83ad39e76f25182691c1
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-06-05 22:11:41 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,59 +1,20 @@
 // Copyright 2021 NNTU-CS
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
 #include <string>
 #include <vector>
-#include <utility>
 
 template <typename T>
 class BST {
   struct Node {
-    T key;
-    int count;
-    Node* left;
-    Node* right;
+    T      key;
+    int    count;
+    Node*  left;
+    Node*  right;
     Node(const T& k): key(k), count(1), left(nullptr), right(nullptr) {}
   };
-
   Node* root;
-
-  int insert(Node*& node, const T& key) {
-    if (!node) {
-      node = new Node(key);
-      return 0;
-    }
-    if (key < node->key) {
-      return 1 + insert(node->left, key);
-    } else if (node->key < key) {
-      return 1 + insert(node->right, key);
-    } else {
-      node->count++;
-      return 0;
-    }
-  }
-
-  Node* find(Node* node, const T& key) const {
-    if (!node) return nullptr;
-    if (key < node->key) return find(node->left, key);
-    if (node->key < key) return find(node->right, key);
-    return node;
-  }
-
-  int depth(Node* node) const {
-    if (!node) return 0;
-    int dl = depth(node->left);
-    int dr = depth(node->right);
-    return 1 + (dl > dr ? dl : dr);
-  }
-
-  void traverse(Node* node, std::vector<std::pair<T,int>>& vec) const {
-    if (!node) return;
-    vec.emplace_back(node->key, node->count);
-    traverse(node->left, vec);
-    traverse(node->right, vec);
-  }
 
   void destroy(Node* node) {
     if (!node) return;
@@ -62,21 +23,63 @@ class BST {
     delete node;
   }
 
+  Node* insert(Node* node, const T& key) {
+    if (!node) {
+      return new Node(key);
+    }
+    if (key < node->key) {
+      node->left = insert(node->left, key);
+    } else if (node->key < key) {
+      node->right = insert(node->right, key);
+    } else {
+      node->count++;
+    }
+    return node;
+  }
+
+  int search(Node* node, const T& key) const {
+    if (!node) return 0;
+    if (key < node->key)        return search(node->left, key);
+    if (node->key < key)        return search(node->right, key);
+    return node->count;
+  }
+
+  int depth(Node* node) const {
+    if (!node) return 0;
+    int dl = depth(node->left);
+    int dr = depth(node->right);
+    return (dl > dr ? dl : dr) + 1;
+  }
+
+  void traverse(Node* node, std::vector<std::pair<T,int>>& elems) const {
+    if (!node) return;
+    traverse(node->left, elems);
+    elems.emplace_back(node->key, node->count);
+    traverse(node->right, elems);
+  }
+
  public:
   BST(): root(nullptr) {}
   ~BST() { destroy(root); }
 
-  void insert(const T& key) { insert(root, key); }
-
-  int search(const T& key) const {
-    Node* nd = find(root, key);
-    return nd ? nd->count : 0;
+  void insert(const T& key) {
+    root = insert(root, key);
   }
 
-  int depth() const { return depth(root); }
+  int search(const T& key) const {
+    return search(root, key);
+  }
 
-  void getAll(std::vector<std::pair<T,int>>& vec) const {
-    traverse(root, vec);
+  int depth() const {
+    int d = depth(root);
+    if (d > 0) return d - 1;
+    return 0;
+  }
+
+  std::vector<std::pair<T,int>> toVector() const {
+    std::vector<std::pair<T,int>> elems;
+    traverse(root, elems);
+    return elems;
   }
 };
 
