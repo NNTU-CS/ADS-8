cb65af49e0c6986ec7829da0cc608b59209ba716
egorisan <epodcherednichenko@gmail.com>
2025-06-07 00:43:18 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> word_tree;
+    const char* filename = "src/war_peace.txt";
+
+    makeTree(word_tree, filename);
+    printFreq(word_tree);
+
     return 0;
 }
