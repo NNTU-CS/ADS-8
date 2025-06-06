b4de8b6a62e150dfca0424a088af65f8534cfa25
shar1123 <shchelkunova.arina2314@mail.ru>
2025-06-06 12:18:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+using namespace std;
+
+void makeTree(BST<string>& tree, const char* filename);
+void printFreq(BST<string>& tree);
 
 int main() {
-    return 0;
+  BST<std::string> tree;
+  const char* filename = "war_peace.txt";
+  makeTree(tree, filename);
+  printFreq(tree);
+  return 0;
 }
