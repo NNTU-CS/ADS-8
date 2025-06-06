23b5ed99155d6de8957e0d1ba95442afc4520a4c
Andrei <140070906+andrr72@users.noreply.github.com>
2025-06-06 21:23:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
 
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  std::cout << "Tree depth: " << tree.depth() << '\n';
+  printFreq(tree);
+  return 0;
 }
