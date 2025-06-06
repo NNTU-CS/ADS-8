1101636c27082743d83fa23d8d286332d1d87f63
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-06 23:14:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
+#include <cstdlib>
+#include <fstream>
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
+  // Создание пустого дерева
+  BST<std::string> tree;
+  const char* filename = "war_peace.txt"; // Путь к файлу для анализа
+
+
+  makeTree(tree, filename);
+
+  std::ofstream outFile("result/freq.txt");
+  if (!outFile) {
+    return EXIT_FAILURE;
+  }
+
+  tree.printSortedByFrequency(outFile);
+
+  return EXIT_SUCCESS;
 }
