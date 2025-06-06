916c18b491e09bd0494223fa263e0a4a1a506c80
malishev-danil <malishev-danil5@yandex.ru>
2025-06-06 22:31:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ void printFreq(const BST<std::string>& tree);
 
 int main() {
   BST<std::string> tree;
-  const char* filename = "war_peace.txt";
+  const char* filename = "https://github.com/malishev-danil/ADS-8/blob/bee66f0926fb93c7ad3a47a0d879178279d7440d/test/war_peace.txt";
   makeTree(tree, filename);
   printFreq(tree);
   return 0;
