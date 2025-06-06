1b9b1203d7168b4ad51fc997ddd5f05dfc0325b2
malishev-danil <malishev-danil5@yandex.ru>
2025-06-06 22:34:24 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,28 +6,28 @@
 void makeTree(BST<std::string>& tree, const char* filename);
 
 TEST(lab8, test1_1) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    const char* filename = "https://github.com/malishev-danil/ADS-8/blob/bee66f0926fb93c7ad3a47a0d879178279d7440d/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int depth = tree.depth();
     EXPECT_EQ(depth, 35);
 }
 TEST(lab8, test1_2) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    const char* filename = "https://github.com/malishev-danil/ADS-8/blob/bee66f0926fb93c7ad3a47a0d879178279d7440d/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("pierre");
     EXPECT_EQ(count, 1963);
 }
 TEST(lab8, test1_3) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    const char* filename = "https://github.com/malishev-danil/ADS-8/blob/bee66f0926fb93c7ad3a47a0d879178279d7440d/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("natasha");
     EXPECT_EQ(count, 1212);
 }
 TEST(lab8, test1_4) {
-    const char* filename = "/home/runner/work/ADS-8/ADS-8/build/test/war_peace.txt";
+    const char* filename = "https://github.com/malishev-danil/ADS-8/blob/bee66f0926fb93c7ad3a47a0d879178279d7440d/test/war_peace.txt";
     BST<std::string> tree;
     makeTree(tree, filename);
     int count = tree.search("andrew");
