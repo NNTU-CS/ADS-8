b9eb40fe2cfc361a903fbb0b219a64387bb67e4c
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-06-06 03:26:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
 
+extern void makeTree(BST<std::string>& tree, const char* filename);
+extern void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> tree;
+
+    makeTree(tree, "src/war_peace.txt");
+
+    printFreq(tree);
+    
     return 0;
 }
