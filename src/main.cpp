cfaa3dafd0356e43631c029fa883e26b5d9f4f0e
malishev-danil <malishev-danil5@yandex.ru>
2025-06-06 20:52:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 // Copyright 2021 NNTU-CS
+#include <string>
+#include <iostream>
 #include "bst.h"
+using std::cout;
+using std::endl;
+using std::string;
+
+void makeTree(BST<string>& tree, const char* filename);
+void printFreq(BST<string>& tree);
 
 int main() {
-    return 0;
+  BST<std::string> tree;
+  const char* filename = "war_peace.txt";
+  makeTree(tree, filename);
+  printFreq(tree);
+  return 0;
 }
