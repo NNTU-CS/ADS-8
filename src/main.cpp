4c7d9856cfca994508a6e30ad7780758cec26efd
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-06-06 10:40:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
-}
+  BST<std::string> tree;
+    
+  std::cout << "Building tree from War and Peace..." << std::endl;
+  makeTree(tree, "src/war_peace.txt");
+
+  std::cout << "Tree depth: " << tree.depth() << std::endl;
+    
+  std::cout << "\nWord frequencies (also saved to result/freq.txt):\n" << std::endl;
+  printFreq(tree);  
+  return 0;
+} 
