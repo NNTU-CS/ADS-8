9cf01245381e6e467554f76abc5c2d4666898a33
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-06-07 17:06:29 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -12,25 +12,20 @@ private:
         int count;
         Node* left;
         Node* right;
-
         Node(T value) : data(value), count(1), left(nullptr), right(nullptr) {}
     };
-
     Node* root;
-
-    // Вспомогательные методы для рекурсивного добавления и поиска
     void insert(Node*& node, const T& value) {
         if (node == nullptr) {
             node = new Node(value);
         } else if (value == node->data) {
-            node->count++;
+            node->count++; // Увеличиваем счетчик при повторении слова
         } else if (value < node->data) {
             insert(node->left, value);
         } else {
             insert(node->right, value);
         }
     }
-
     bool search(Node* node, const T& value) const {
         if (node == nullptr) {
             return false;
@@ -42,7 +37,6 @@ private:
             return search(node->right, value);
         }
     }
-
     void printFreq(Node* node) const {
         if (node == nullptr) {
             return;
@@ -51,7 +45,6 @@ private:
         std::cout << node->data << ": " << node->count << std::endl;
         printFreq(node->right);
     }
-
     void depth(Node* node, int& max_depth, int current_depth) const {
         if (node == nullptr) {
             return;
@@ -65,27 +58,21 @@ private:
 
 public:
     BST() : root(nullptr) {}
-
     void insert(const T& value) {
         insert(root, value);
     }
-
     bool search(const T& value) const {
         return search(root, value);
     }
-
     void printFreq() const {
         printFreq(root);
     }
-
     int depth() const {
         int max_depth = 0;
         depth(root, max_depth, 1);
         return max_depth;
     }
-
     ~BST() {
-        // Очистка памяти
         delete root;
     }
 };
