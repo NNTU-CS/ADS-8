9eedb46d8aba984db933bf09f58c759a315ed187
Lix <zelenovaaleksandra2@gmail.com>
2025-06-07 23:26:06 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -5,130 +5,128 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 template<typename T>
 struct Node {
-  T key;
-  size_t count;
-  Node *left;
-  Node *right;
+    T key;
+    size_t count;
+    Node *left;
+    Node *right;
 
-  Node(const T& k) : key(k), count(1), left(nullptr), right(nullptr) {}
+    explicit Node(const T &k) : key(k), count(1), left(nullptr), right(nullptr) {}
 };
 
 template<typename T>
 class BST {
 private:
-  Node<T>* root;
+    Node<T> *root;
 
 public:
-  BST() : root(nullptr) {}
-  ~BST();
-  bool insert(const T&);
-  bool search(const T&);
-  size_t depth();
-  void inorder(std::vector<Node<T>*>&);
-  void printFrequency();
+    BST() : root(nullptr) {}
+    ~BST();
+    bool insert(const T &val);
+    bool search(const T &val);
+    size_t depth();
+    void inorder(std::vector<Node<T> *> &vec);
+    void printFrequency();
 };
 
 template<typename T>
 BST<T>::~BST() {
-  auto destroy = [](Node<T>* node) {
-    if(node != nullptr){
-      destroy(node->left);
-      destroy(node->right);
-      delete node;
-    }
-  };
-  destroy(root);
+    auto destroy = [&](Node<T> *node) {
+        if (node != nullptr) {
+            destroy(node->left);
+            destroy(node->right);
+            delete node;
+        }
+    };
+    destroy(root);
 }
 
 template<typename T>
-bool BST<T>::insert(const T& val) {
-  if (root == nullptr) {
-    root = new Node<T>(val);
+bool BST<T>::insert(const T &val) {
+    if (root == nullptr) {
+        root = new Node<T>(val);
+        return true;
+    }
+
+    Node<T> *current = root;
+    while (true) {
+        if (val == current->key) {
+            ++current->count;
+            break;
+        }
+
+        if (val < current->key) {
+            if (current->left == nullptr) {
+                current->left = new Node<T>(val);
+                break;
+            } else {
+                current = current->left;
+            }
+        } else {
+            if (current->right == nullptr) {
+                current->right = new Node<T>(val);
+                break;
+            } else {
+                current = current->right;
+            }
+        }
+    }
     return true;
-  }
-
-  Node<T>* current = root;
-  while(true) {
-    if(val == current->key){
-      ++current->count;
-      break;
-    }
-
-    if(val < current->key) {
-      if(current->left == nullptr) {
-        current->left = new Node<T>(val);
-        break;
-      }
-      else{
-        current = current->left;
-      }
-    }
-    else {
-      if(current->right == nullptr) {
-        current->right = new Node<T>(val);
-        break;
-      }
-      else{
-        current = current->right;
-      }
-    }
-  }
-  return true;
 }
 
 template<typename T>
-bool BST<T>::search(const T& val) {
-  Node<T>* current = root;
-  while(current != nullptr && current->key != val) {
-    if(val < current->key) {
-      current = current->left;
+bool BST<T>::search(const T &val) {
+    Node<T> *current = root;
+    while (current != nullptr && current->key != val) {
+        if (val < current->key) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
     }
-    else {
-      current = current->right;
+    return current != nullptr;
+}
+
+template<typename T>
+size_t BST<T>::depth() {
+    if (root == nullptr) return 0;
+    return 1 + std::max(depthHelper(root->left), depthHelper(root->right));
+}
+
+template<typename T>
+size_t BST<T>::depthHelper(Node<T> *node) {
+    if (node == nullptr) return 0;
+    return 1 + std::max(depthHelper(node->left), depthHelper(node->right));
+}
+
+template<typename T>
+void BST<T>::inorder(std::vector<Node<T> *> &vec) {
+    inorderHelper(vec, root);
+}
+
+template<typename T>
+void BST<T>::inorderHelper(std::vector<Node<T> *> &vec, Node<T> *node) {
+    if (node != nullptr) {
+        inorderHelper(vec, node->left);
+        vec.push_back(node);
+        inorderHelper(vec, node->right);
     }
-  }
-  return current != nullptr;
-}
-
-template<typename T>
-size_t BST<T>::depth(Node<T>* node) {
-  if(node == nullptr) return 0;
-  return 1 + std::max(depth(node->left), depth(node->right));
-}
-
-template<typename T>
-size_t BST<T>::depth(){
-  return depth(root);
-}
-
-template<typename T>
-void BST<T>::inorder(std::vector<Node<T>*>& vec, Node<T>* node) {
-  if(node != nullptr) {
-    inorder(vec, node->left);
-    vec.push_back(node);
-    inorder(vec, node->right);
-  }
-}
-
-template<typename T>
-void BST<T>::inorder(std::vector<Node<T>*>& vec) {
-  inorder(vec, root);
 }
 
 template<typename T>
 void BST<T>::printFrequency() {
-  std::vector<Node<T>*> nodes;
-  inorder(nodes);
-  std::sort(nodes.begin(), nodes.end(),
-            [](const Node<T>* a, const Node<T>* b) {
-              return a->count > b->count || (a->count == b->count && a->key < b->key);
-            });
+    std::vector<Node<T> *> nodes;
+    inorder(nodes);
+    std::sort(nodes.begin(), nodes.end(), [](const Node<T> *a, const Node<T> *b) {
+        return a->count > b->count || (a->count == b->count && a->key < b->key);
+    });
 
-  for(auto n : nodes) {
-    std::cout << n->key << ": " << n->count << "\n";
-  }
+    for (auto n : nodes) {
+        std::cout << n->key << ": " << n->count << "\n";
+    }
 }
+
 #endif  // INCLUDE_BST_H_
