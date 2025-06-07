768f31ea1bd7cd1bd34765248aeba2ed16353ea4
Lix <zelenovaaleksandra2@gmail.com>
2025-06-07 23:27:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,46 +4,49 @@
 #include  <locale>
 #include  <cstdlib>
 #include <cctype>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include "bst.h"
 
-using namespace std;
+using std::string;
+using std::vector;
 
-void makeTree(BST<string>& tree, const char* filename) {
-  ifstream file(filename);
-  if (!file.is_open()) {
-    cerr << "Error opening the file!\n";
-    return;
-  }
+void makeTree(BST<string> &tree, const char *filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening the file!\n";
+        return;
+    }
 
-  string word;
-  char c;
-  while(file.get(c)) {
-    if(isalpha(c)){
-      word += tolower(c);
+    string word;
+    char c;
+    while (file.get(c)) {
+        if (isalpha(c)) {
+            word += static_cast<char>(tolower(c));
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
     }
-    else if(word.length() > 0) {
-      tree.insert(word);
-      word.clear();
-    }
-  }
-  file.close();
+    file.close();
 }
 
-void printFreq(BST<string>& tree) {
-  ofstream output("result/freq.txt");
-  if(output.fail()){
-    cerr << "Error writing to file!\n";
-    return;
-  }
+void printFreq(BST<string> &tree) {
+    std::ofstream output("result/freq.txt");
+    if (output.fail()) {
+        std::cerr << "Error writing to file!\n";
+        return;
+    }
 
-  vector<Node<string>*> nodes;
-  tree.inorder(nodes);
-  sort(nodes.begin(), nodes.end(), [](const Node<string>* a, const Node<string>* b) {
-    return a->count > b->count || (a->count == b->count && a->key < b->key);
-  });
+    vector<Node<string> *> nodes;
+    tree.inorder(nodes);
+    std::sort(nodes.begin(), nodes.end(), [](const Node<string> *a, const Node<string> *b) {
+        return a->count > b->count || (a->count == b->count && a->key < b->key);
+    });
 
-  for(auto n : nodes) {
-    output << n->key << ": " << n->count << endl;
-  }
-  output.close();
+    for (auto n : nodes) {
+        output << n->key << ": " << n->count << '\n';
+    }
+    output.close();
 }
