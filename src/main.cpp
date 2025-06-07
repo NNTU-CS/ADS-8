84401c39c0c5df6d67f669adcb28068569c79f9e
Tripleez <art_khorosh@vk.com>
2025-06-08 00:28:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "src/war_peace.txt";
+    
+    makeTree(tree, filename);
+    printFreq(tree);
+    
     return 0;
 }
