865a5081c219791e26d8ae8806f3b38c488940a6
methevodichka <145225062+methevodichka@users.noreply.github.com>
2025-06-07 23:20:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-// Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
+
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "war_peace.txt");
+
+  std::cout << "BST depth: " << tree.depth() << std::endl;
+
+  printFreq(tree);
+
+  return 0;
 }
