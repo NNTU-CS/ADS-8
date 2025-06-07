2d634a9ac2d7981604dac86f23baac98f3e2e515
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-06-07 17:07:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,10 @@
 int main() {
     BST<std::string> tree;
     const char* filename = "src/war_peace.txt";
-
-    // Создаем дерево с словами из файла
     makeTree(tree, filename);
-
-    // Выводим частотный анализ
     std::cout << "Word frequency analysis:\n";
     tree.printFreq();
-
-    // Печатаем результаты в файл
     printFreq(tree);
-
-    // Выводим глубину дерева
     std::cout << "\nTree depth: " << tree.depth() << std::endl;
-
     return 0;
 }
