a2d38a37c2eb14b79dc99be05044dc0404b47786
Lix <zelenovaaleksandra2@gmail.com>
2025-06-07 23:15:34 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include "alg.cpp"
 #include "bst.h"
 
+using namespace std;
+
 int main() {
+    BST<string> words;
+    makeTree(words, "src/war_and_peace.txt");
+    cout << "Tree constructed.\n";
+    cout << "Maximum tree depth: " << words.depth() << endl;
+    printFreq(words);
+    cout << "Result saved in 'result/freq.txt'\n";
     return 0;
 }
