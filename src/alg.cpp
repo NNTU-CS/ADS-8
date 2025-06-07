4aa0a3e811bd38a33022011c59058dbf0ac77513
Lix <zelenovaaleksandra2@gmail.com>
2025-06-07 23:13:39 +0300

    Update alg.cpp8

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,43 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  "bst.h"
+#include <cctype>
+#include "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+using namespace std;
+
+void makeTree(BST<string>& tree, const char* filename) {
+  ifstream file(filename);
+  if (!file.is_open()) {
+    cerr << "Error opening the file!\n";
+    return;
+  }
+  string word;
+  char c;
+  while(file.get(c)) {
+    if(isalpha(c)){
+      word += tolower(c);
+    }
+    else if(word.length() > 0) {
+      tree.insert(word);
+      word.clear();
+    }
+  }
+  file.close();
+}
+void printFreq(BST<string>& tree) {
+  ofstream output("result/freq.txt");
+  if(output.fail()){
+    cerr << "Error writing to file!\n";
+    return;
+  }
+  vector<Node<string>*> nodes;
+  tree.inorder(nodes);
+  sort(nodes.begin(), nodes.end(), [](const Node<string>* a, const Node<string>* b) {
+    return a->count > b->count || (a->count == b->count && a->key < b->key);
+  });
+  for(auto n : nodes) {
+    output << n->key << ": " << n->count << endl;
+  }
+  output.close();
 }
