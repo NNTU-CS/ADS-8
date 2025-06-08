d3dde332a495a6e061ee4404a163ec661afd1812
vkupriyanova <vika2018arz@gmail.com>
2025-06-08 11:17:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,14 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
-#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include <cctype>
 #include <string>
+#include <algorithm>
+#include "bst.h"
 void makeTree(BST<std::string>& tree, const char* filename) {
-  std::ifstream file (filename);
+  std::ifstream file(filename);
   if (!file) {
     std::cout << "File error!" << std::endl;
     return;
@@ -15,10 +16,10 @@ void makeTree(BST<std::string>& tree, const char* filename) {
   std::string word;
   char ch;
   while (file.get(ch)) {
-    if (std::isalpha(ch)) {
-      word += std::tolower(ch);
+    if (std::isalpha(static_cast<unsigned char>(ch))) {
+      word += std::tolower(static_cast<unsigned char>(ch));
     } else {
-      if (word.empty()) {
+      if (!word.empty()) {
         tree.insert(word);
         word.clear();
       }
@@ -29,16 +30,16 @@ void makeTree(BST<std::string>& tree, const char* filename) {
   }
   file.close();
 }
-void printFreq (BST<std::string>& tree) {
+void printFreq(BST<std::string>& tree) {
   auto items = tree.getAll();
   std::sort(items.begin(), items.end(),
-          [] (const auto& a, const auto& b) {
-            return a.second > b.second;
-          });
+            [](const auto& a, const auto& b) {
+              return a.second > b.second;
+            });
   std::ofstream fout("result/freq.txt");
-    for (const auto& pair : items) {
-        std::cout << pair.first << " : " << pair.second << std::endl;
-        fout << pair.first << " : " << pair.second << "\n";
+  for (const auto& pair : items) {
+    std::cout << pair.first << " : " << pair.second << std::endl;
+    fout << pair.first << " : " << pair.second << "\n";
   }
-  fout.close();    
+  fout.close();
 }
