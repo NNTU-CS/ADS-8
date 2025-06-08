db0465be2182ad99997116e1d728c62236a02d34
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-06-08 21:53:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "../include/bst.h"
+#include <iostream>
+#include <string>
+
+void makeTree(BST<std::string>& bstInstance, const char* inputFile);
+void printFreq(const BST<std::string>& bstInstance);
 
 int main() {
-    return 0;
+  BST<std::string> wordTree;
+  const char* sourceFile = "war_peace.txt";
+
+  makeTree(wordTree, sourceFile);
+  printFreq(wordTree);
+
+  return 0;
 }
