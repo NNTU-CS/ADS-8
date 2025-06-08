201c3391294728e49a79063ef25dc1ca9e5f97e3
DodonovaAnna <annadodonova9723@gmail.com>
2025-06-08 22:47:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,17 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
-#include <cctype>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <utility>
+#include  <locale>
+#include  <cstdlib>
+#include  <cctype>
+#include  <algorithm>
+#include  <vector>
+#include  <string>
+#include  <utility>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
   std::ifstream file(filename);
-  if (!file) {
-      std::cout << "File error!" << std::endl;
-      return;
-  }
   std::string Word;
   while (!file.eof()) {
       char ch = file.get();
