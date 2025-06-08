77a7e39150de5ddb6c99b1a2efbacc18a8f851c4
vkupriyanova <vika2018arz@gmail.com>
2025-06-08 10:53:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,38 @@
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file (filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return;
+  }
+  std::string word;
+  char ch;
+  while (file.get (ch)) {
+    if (std::isalpha(ch)) {
+      word += std::tolower(ch);
+    } else {
+      if (word.empty()) {
+        tree.insert(word);
+        word.clear();
+      }
+    }
+  }
+  if (!word.empty()) {
+    tree.insert(word);
+  }
+  file.close();
+}
+void printFreq (BST<std::string>& tree) {
+  auto items = tree.getAll();
+  std::sort(items.begin(), items.end(),
+          [] (const auto& a, const auto& b) {
+            return a.second > b.second;
+          });
+  std::ofstream fout("result/freq.txt");
+    for (const auto& pair : items) {
+        std::cout << pair.first << " : " << pair.second << std::endl;
+        fout << pair.first << " : " << pair.second << "\n";
+  }
+  fout.close();    
 }
