ee21302719d05bc8f43cdc6bb33fd5106b7d7640
dan4ku <danilagavr121@gmail.com>
2025-06-08 20:03:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,30 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
+#include <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error!" << std::endl;
+        return;
+    }
+    std::string word;
+    while (!file.eof()) {
+        char ch = file.get();
+        if (ch >= 'A' && ch <= 'Z') ch += 'a' - 'A';
+        if (ch >= 'a' && ch <= 'z') {
+            word += ch;
+        } else {
+            if (!word.empty())
+              tree.add(word);
+            word.clear();
+        }
+    }
+    file.close();
 }
