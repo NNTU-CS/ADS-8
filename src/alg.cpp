1655d6413bf17b5414c267f14c012d5f07c38519
Kuchumova_Ekaterina <e_kuchumova_06@mail.ru>
2025-06-08 20:20:47 +0300

    Update alg_1.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,39 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <regex>
+#include <algorithm>
+#include <vector>
+
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    std::string word;
+    std::regex word_regex("[a-zA-Z]+");
+
+    while (file >> word) {
+        std::sregex_iterator it(word.begin(), word.end(), word_regex);
+        std::sregex_iterator end;
+
+        for (; it != end; ++it) {
+            std::string clean_word = it->str();
+            std::transform(clean_word.begin(), clean_word.end(), /
+              clean_word.begin(), ::tolower);
+            tree.insert(clean_word);
+        }
+    }
+}
+void printFreq(BST<std::string>& tree) {
+    auto words = tree.toVector();
+
+    std::sort(words.begin(), words.end(),
+        [](const auto& a, const auto& b) {
+            return a.second > b.second; // по убыванию частоты
+        });
+
+    std::ofstream out("result/freq.txt");
+    for (const auto& [word, count] : words) {
+        std::cout << word << ": " << count << '\n';
+        out << word << ": " << count << '\n';
+    }
 }
