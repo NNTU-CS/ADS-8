8cbcfa23e7f43f9b47368226a252c97b361f4d26
KorobovaKate <kat3kor@yandex.ru>
2025-06-08 19:02:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,14 @@
 #include <string>
 #include "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename);
-void printFreq(const BST<std::string>& tree);
-
 int main() {
-    BST<std::string> wordTree;
+    BST<std::string> tree;
     const char* filename = "src/war_peace.txt";
-
-    makeTree(wordTree, filename);
-
-    std::cout << "Глубина дерева: " << wordTree.depth() << std::endl;
-
-    printFreq(wordTree);
-
-    int pierreCount = wordTree.search("pierre");
-    std::cout << "Pierres найдено: " << pierreCount << std::endl;
-
-    int natashaCount = wordTree.search("natasha");
-    std::cout << "Natashas найдено: " << natashaCount << std::endl;
-
-    int andrewCount = wordTree.search("andrew");
-    std::cout << "Andrews найдено: " << andrewCount << std::endl;
-
+    
+    makeTree(tree, filename);
+    printFreq(tree);
+    
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    
     return 0;
 }
