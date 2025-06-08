a4b52fcbecc351bf42a8d44876c9f76af2472b41
Vegsdo21 <sgt.pepper21@mail.ru>
2025-06-08 23:05:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ void printFreq(BST<std::string>& tree);
 
 int main() {
   BST<std::string> tree;
-  const char* filename = "war_peace.txt";
+  const char* filename = "src/war_peace.txt";
   makeTree(tree, filename);
   std::ofstream outFile("result/freq.txt");
   if (!outFile) return EXIT_FAILURE;
