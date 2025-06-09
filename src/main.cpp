da551436b6f7cbed82372721480a7a713f8e2a9c
ViktorKondrashin <visktor.srgeev@mail.ru>
2025-06-09 03:47:46 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ void makeTree(BST<std::string>&, const char*);
 void printFreq(BST<std::string>&);
 
 int main() {
-	BST<std::string> tree;
-	makeTree(tree, "war_peace.txt");
-	printFreq(tree);
-	std::cout << "Depth: " << tree.depth() << '\n';
-	return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  printFreq(tree);
+  std::cout << "Depth: " << tree.depth() << '\n';
+  return 0;
 }
