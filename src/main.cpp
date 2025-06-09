1dac885367c1d2f4362b748f88ab74f6ed1b02da
ViktorKondrashin <visktor.srgeev@mail.ru>
2025-06-09 03:33:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  printFreq(tree);
+  std::cout << "Depth: " << tree.depth() << "\n";
+  return 0;
 }
